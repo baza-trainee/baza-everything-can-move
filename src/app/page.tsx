53fc70ff5d58d562272579eef40f0392e4850c7b
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-10-21 17:23:01 +0300

    доробив бігучу доріжку

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Marquee from '../components/Marquee/Marquee';
+import Marquee from '../components/sections/Home/Marquee/Marquee';
 import Preloader from '../components/Preloader/Preloader';
+import ColoredGirl from '../components/Girl/ColoredGirl';
+
 export default function Home() {
   return (
     <>
-      <Marquee/>
+      <Marquee />
+      <ColoredGirl />
       <Preloader />
     </>
   );
--- src/components/sections/Home/Marquee/Marquee.tsx
+++ src/components/sections/Home/Marquee/Marquee.tsx
@@ -1,18 +1,23 @@
 "use client"; // Додайте це, якщо використовуєте Next.js
 
 import React from "react";
-import Marquee from "react-double-marquee"; // Переконайтеся, що назва правильна
+import Marquee from "react-double-marquee";
 import "./marquee.css";
+
 export default function FooComponent() {
   return (
-    <div className="w-full whitespace-nowrap overflow-hidden marque-item">
+    <div className="relative w-full whitespace-nowrap overflow-hidden marque-item">
       <Marquee direction="left">
+        <span className="absolute-highlight"></span>
+        baza trainee baza trainee
         baza trainee baza trainee baza trainee baza trainee baza trainee baza
-        trainee baza trainee baza trainee baza trainee
+        trainee baza trainee
       </Marquee>
       <Marquee direction="right">
-        baza trainee baza trainee baza trainee baza trainee baza trainee baza
+        baza trainee baza trainee
+        baza trainee baza trainee baza trainee baza
         trainee baza trainee baza trainee baza trainee
+        <span className="absolute-highlight"></span>
       </Marquee>
     </div>
   );
