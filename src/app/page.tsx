0b89fe977aaf1eddf7b140a2cb9a3383c030e601
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-10-24 21:40:41 +0300

    removed the preload. did made an animation for the OldBase section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import Hero from '../components/sections/Home/Hero/Hero';
 import Marquee from '../components/sections/Home/Marquee/Marquee';
-import Preloader from '../components/Preloader/Preloader';
+// import Preloader from '../components/Preloader/Preloader';
 import ScrollButton from '../components/ui/ScrollButton/ScrollButton';
+import OldBase from '../components/sections/Home/OldBase/OldBase';
 
 export default function Home() {
   return (
     <>
       <Hero/>
       <Marquee />
-      <Preloader />
+      {/* <Preloader /> */}
       <ScrollButton/>
+      <OldBase/>
     </>
   );
 }
