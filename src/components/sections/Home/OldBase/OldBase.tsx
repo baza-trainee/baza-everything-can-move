0a5fd6e3ba781c6b9853c62244b47105e41dbc90
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-10-25 12:03:37 +0300

    the start section of the base is made

--- src/components/sections/Home/OldBase/OldBase.tsx
+++ src/components/sections/Home/OldBase/OldBase.tsx
@@ -3,18 +3,23 @@ import "./style.css";
 
 export default function OldBase() {
   return (
-    <section className="oldBase pt-[114px]">
+    <section className="oldBase pt-[114px] mb-[114px]">
       <div className="container">
         <h3 className="text-[48px] leading-[150%] uppercase">старт бази</h3>
-        <span className="leading-[150%]">квітень 2023</span>
-        <div className="flex justify-between">
-          <div className="orbit">
+        <p className="leading-[150%] relative left-5 mb-8">
+          <span
+            className="absolute top-2 left-[-2%]  w-2 h-2 rounded-full"
+            style={{ backgroundColor: "#D3FD50" }}></span>
+          квітень 2023
+        </p>
+        <div className="flex items-center justify-end gap-52">
+          <div className="orbit relative flex w-[18vw] justify-center items-center">
             <div className="orbit-circle big-orbit"></div>
             <div className="orbit-circle middle-orbit"></div>
+            <div className="orbit-circle middle-orbit-1"></div>
             <div className="orbit-circle small-orbit _1"></div>
             <div className="orbit-circle middle-orbit-2"></div>
             <div className="orbit-circle small-orbit _3"></div>
-            {/* <div className="orbit-circle small-orbit _2"></div> */}
             <div className="orbit-circle small-orbit _4"></div>
           </div>
           <div className="w-[400px]">
@@ -22,12 +27,12 @@ export default function OldBase() {
               Квітень 2023 року став стартом написання проєктів на Базі.
               <br />
               <br />
-              На тойчас проєкти були внутрішніми, але дуже швидко до нас набігла ОООТАКА КУПА замовників.
+              На тойчас проєкти були внутрішніми, але дуже швидко до нас набігла
+              ОООТАКА КУПА замовників.
               <br />
               <br />
-              А ще через два місяці до нас звернулись
-              перші роботодавці (із міста Запоріжжя) і запросили на роботу першу
-              ТЕСТУВАЛЬНИЦЮ.
+              А ще через два місяці до нас звернулись перші роботодавці (із
+              міста Запоріжжя) і запросили на роботу першу ТЕСТУВАЛЬНИЦЮ.
               <br />
               <br />
               Так почався наш цикл СТАЖУВАННЯ - ПРАЦЕВЛАШТУВАННЯ.
