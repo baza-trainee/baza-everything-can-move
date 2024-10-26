a545ae26cb064f095d609ad76b7d6036b27420c4
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-10-26 16:05:52 +0300

    add section Today + adaprive sites

--- src/components/ui/ScrollButton/ScrollButton.tsx
+++ src/components/ui/ScrollButton/ScrollButton.tsx
@@ -5,7 +5,7 @@ export default function ScrollButton() {
     <button
       // onClick={openModal}
       type="button"
-      className="fixed right-[50px] btn-support mr-[24px] h-[132px] w-[132px] animate-pulse rounded-full bg-accent text-center text-m font-medium uppercase text-white custom-shadow transition-all duration-300 hover:bg-hover tablet:flex">
+      className="fixed right-[50px] btn-support mr-[24px] h-[132px] w-[132px] bottom-4 animate-pulse rounded-full bg-accent text-center text-m font-medium uppercase text-white custom-shadow transition-all duration-300 hover:bg-hover flex items-center justify-center z-10">
       contact
     </button>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         foreground: "var(--foreground)",
       },
     },
+    screens: {
+      smallMobile: "320px",
+      // => @media (min-width: 320px) { ... }
+      mobile: "375px",
+      // => @media (min-width: 375px) { ... }
+      tablet: "744px",
+      // => @media (min-width: 744px) { ... }
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+      desktop: "1440px",
+      // => @media (min-width: 1440px) { ... }
+    },
   },
   plugins: [],
 };
