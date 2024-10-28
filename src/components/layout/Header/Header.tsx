9df87e6ae205fb4260e0519e4aa770622ddb309d
GannaKov <zlatta2000@gmail.com>
2024-10-28 19:28:06 +0100

    change mobile

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { ICONS_SRC } from '@/constants/icons/iconsSrc';
 
@@ -34,31 +35,46 @@ const Header = () => {
     setOpenMenu(!openMenu);
   };
   return (
-    <header>
+    <header className="flex justify-between">
       {!openMenu && (
-        <div className="text:md lg:text-l leading-o-120 flex justify-end uppercase">
-          <div className="text-olga-green-extra bg-olga-btn-menu flex h-[50px] w-[100px] items-end p-3 lg:h-[80px] lg:w-[174px]">
-            {indexOfCurrentPage >= 0
-              ? NAV_LINKS[indexOfCurrentPage].name
-              : 'UPS'}
-          </div>
+        <>
           <Link
-            href={NAV_LINKS[indexOfNextPage].link}
-            className="hover:text-olga-green/90  bg-olga-btn-menu flex h-[90px] w-[139px] items-end p-3 text-white transition-colors duration-300 ease-linear lg:h-[120px] lg:w-[344px]"
-          >
-            {indexOfNextPage >= 0 ? NAV_LINKS[indexOfNextPage].name : 'UPS'}
-          </Link>
-          <div
+            href="/"
             onClick={handleMenuClick}
-            className="bg-olga-btn-menu hover:bg-olga-green hover:text-olga-bg h-[130px] w-[125px] cursor-pointer px-3 pb-3 pt-6 text-white transition-colors duration-300 ease-linear lg:h-[160px] lg:w-[236px]"
+            className="pl-5 pt-2 lg:pl-10 lg:pt-10"
           >
-            <ICONS_SRC.OPEN_MENU_ICON className="mb-[50px] ml-auto w-16 fill-current transition-colors duration-300 ease-linear hover:fill-black lg:mb-[77px]" />
-            {/* <OpenMenuIcon className="mb-[77px] ml-auto w-16 fill-current transition-colors duration-300 ease-linear hover:fill-black" /> */}
-            <p className="ml-auto transition-colors duration-300 ease-linear">
-              Меню
-            </p>
+            <Image
+              src="/assets/images/Logo/logo_baza.png"
+              alt="Logo Baza Trainee Ukraine"
+              width={80}
+              height={80}
+              priority
+            />
+          </Link>
+          <div className="text-l leading-o-120 flex uppercase">
+            <div className="text-olga-green-extra bg-olga-btn-menu hidden items-end p-3 xl:flex xl:h-[80px] xl:w-[174px]">
+              {indexOfCurrentPage >= 0
+                ? NAV_LINKS[indexOfCurrentPage].name
+                : 'UPS'}
+            </div>
+            <Link
+              href={NAV_LINKS[indexOfNextPage].link}
+              className="hover:text-olga-green/90 bg-olga-btn-menu hidden items-end p-3 text-white transition-colors duration-300 ease-linear xl:flex xl:h-[120px] xl:w-[344px]"
+            >
+              {indexOfNextPage >= 0 ? NAV_LINKS[indexOfNextPage].name : 'UPS'}
+            </Link>
+            <div
+              onClick={handleMenuClick}
+              className="bg-olga-btn-menu hover:bg-olga-green hover:text-olga-bg h-[130px] w-[125px] cursor-pointer px-3 pb-3 pt-6 text-white transition-colors duration-300 ease-linear xl:h-[160px] xl:w-[236px]"
+            >
+              <ICONS_SRC.OPEN_MENU_ICON className="mb-[50px] ml-auto w-16 fill-current transition-colors duration-300 ease-linear hover:fill-black xl:mb-[77px]" />
+              {/* <OpenMenuIcon className="mb-[77px] ml-auto w-16 fill-current transition-colors duration-300 ease-linear hover:fill-black" /> */}
+              <p className="text-l ml-auto transition-colors duration-300 ease-linear">
+                Меню
+              </p>
+            </div>
           </div>
-        </div>
+        </>
       )}
 
       {openMenu && (
@@ -73,19 +89,4 @@ const Header = () => {
 };
 export default Header;
 
-//---- mob menu
-// const [openMobileMenu, setOpenMobileMenu] = useState({
-//   right: false,
-// });
-
-// const toggleDrawer = (anchor, open) => (event) => {
-//   if (
-//     event.type === 'keydown' &&
-//     (event.key === 'Tab' || event.key === 'Shift')
-//   ) {
-//     return;
-//   }
-
-//   setOpenMobileMenu({ ...openMobileMenu, [anchor]: open });
-// };
-// --- end mobile menu
+//h-[50px] w-[100px]  ....h-[90px] w-[139px] Link logo pt-4 in main div text:md lg:text-l
