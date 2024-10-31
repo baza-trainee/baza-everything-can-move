5b669648fe7b06c1a8c6108a8ff5f45d933e0fcc
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-10-31 20:49:57 +0100

    Update Subtitle.tsx

--- src/components/ui/Subtitle.tsx
+++ src/components/ui/Subtitle.tsx
@@ -1,24 +1,24 @@
-type Props = {
-  text: string;
-  typeText: 'month' | 'year';
-  year: number;
-};
+import clsx from 'clsx';
+import { ICONS_SHARED } from '@/constants/icons/iconsSrc';
 
-function Subtitle({ text, typeText, year }: Props) {
-  return (
-    <p className="before:content-[' '] relative pl-3 text-[12px] leading-normal before:absolute before:left-0 before:top-2 before:h-[6px] before:w-[6px] before:rounded-full before:bg-olga-green lg:text-s">
-      {typeText === 'month' && (
-        <>
-          {text} {year}
-        </>
-      )}
-      {typeText === 'year' && (
-        <>
-          {year} {text}
-        </>
-      )}
-    </p>
-  );
+interface SubTitleProps {
+  children: React.ReactNode;
+  className?: string;
 }
 
-export default Subtitle;
+export default function SubTitle({ children, className }: SubTitleProps) {
+  return (
+    <div
+      className={clsx(
+        'flex items-center font-main-family text-xs font-regular leading-o-150 text-white lg:text-s',
+
+        className
+      )}
+    >
+      <ICONS_SHARED.ELIPSE_SUB_TITLE className="mr-1 2xl:mr-[6px]" />
+      <span>{children}</span>
+    </div>
+  );
+}
+// Example how to use
+// <SubTitle className="mb-8"> Долучайтесь</SubTitle>
