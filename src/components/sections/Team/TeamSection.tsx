86e6a4bef462f3797094ac6a4bd659c5dfdf4a46
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-10-31 23:36:31 +0100

    Update TeamSection.tsx

--- src/components/sections/Team/TeamSection.tsx
+++ src/components/sections/Team/TeamSection.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 import React from 'react';
 import FotoSwiper from '../../ui/SwiperFoto/FotoSwiper';
 import SectionTitle from '@/components/ui/SectionTitle';
-import SubTitle from '@/components/ui/SubTitle';
 import { teamImages } from './ArrayTeamImages';
+// import SubTitle from '@/components/ui/SubTitle';
 
 function TeamSection() {
   return (
@@ -29,7 +29,7 @@ function TeamSection() {
             <SectionTitle className="pb-1 text-lg lg:text-[2.25rem] 2xl:text-[48px]">
               Команди
             </SectionTitle>
-            <SubTitle>{'квітень 2023'}</SubTitle>
+            {/* <SubTitle>{'квітень 2023'}</SubTitle> */}
           </div>
           <div className="flex flex-col items-center gap-[100px] lg:flex-row lg:justify-between lg:gap-0">
             <div className="w-full lg:max-w-[336px] 2xl:max-w-[486px]">
