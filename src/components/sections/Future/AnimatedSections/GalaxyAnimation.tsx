43477dde076d5bc6ec4b476d60787e4ac8661d62
bohdanalu <b_dana@ukr.net>
2024-10-31 12:42:15 +0200

    refactor: change some points in Galaxy anumation

--- src/components/sections/Future/AnimatedSections/GalaxyAnimation.tsx
+++ src/components/sections/Future/AnimatedSections/GalaxyAnimation.tsx
@@ -13,14 +13,14 @@ function Galaxy() {
     const colors = new Float32Array(pointCount * 3);
 
     for (let i = 0; i < pointCount; i++) {
-      const radius = Math.pow(Math.random(), 1.5) * 5; // Slightly increase radius for more dispersion
+      const radius = Math.pow(Math.random(), 1.3) * 5; // Slightly increase radius for more dispersion
       const branch = i % 3;
       const branchAngle = branch * ((2 * Math.PI) / 3); // Number of galaxy arms
-      const spinAngle = radius * 1.5; // Rotation around center
+      const spinAngle = radius * 1.3; // Rotation around center
 
-      const randomX = (Math.random() - 0.5) * 1.5; // Increase the range for more dispersion
-      const randomY = (Math.random() - 0.5) * 1.5; // Increase the range for more dispersion
-      const randomZ = (Math.random() - 0.5) * 1.5; // Increase the range for more dispersion
+      const randomX = (Math.random() - 0.3) * 1.3; // Increase the range for more dispersion
+      const randomY = (Math.random() - 0.3) * 1.3; // Increase the range for more dispersion
+      const randomZ = (Math.random() - 0.3) * 1.3; // Increase the range for more dispersion
 
       const x = Math.cos(branchAngle + spinAngle) * radius + randomX;
       const y = randomY * 0.1; // Slight spread along Y-axis
@@ -28,8 +28,8 @@ function Galaxy() {
 
       positions.set([x, y, z], i * 3);
 
-      const insideColor = new THREE.Color(0x78ff75);
-      const outsideColor = new THREE.Color(0x311599);
+      const insideColor = new THREE.Color(0x9aff75);
+      const outsideColor = new THREE.Color(0x366346);
       const color = insideColor.lerp(outsideColor, radius / 5);
       colors.set([color.r, color.g, color.b], i * 3);
     }
@@ -51,7 +51,7 @@ function Galaxy() {
 
   useFrame((state, delta) => {
     if (pointsRef.current) {
-      pointsRef.current.rotation.y += delta * 0.075; // Increase rotation speed
+      pointsRef.current.rotation.y += delta * 0.9; // Increase rotation speed
     }
   });
 
@@ -61,9 +61,9 @@ function Galaxy() {
 export default function GalaxyAnimation() {
   return (
     <div className="h-[100%] w-[100%] px-[10%]">
-      <Canvas camera={{ position: [4, 2, 5], fov: 50 }}>
+      <Canvas dpr={0.8} camera={{ position: [0, 10, 4], fov: 50 }}>
         <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 5, 5]} intensity={1.5} />
+        <directionalLight position={[4, 4, 4]} intensity={1.5} />
         <Galaxy />
         <OrbitControls enableDamping={true} />
       </Canvas>
