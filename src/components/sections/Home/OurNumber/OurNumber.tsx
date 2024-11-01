8dee766a013e6bb0966029e7128c28c19411bb3b
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-11-01 15:24:13 +0200

    added adaptive section Ournumber

--- src/components/sections/Home/OurNumber/OurNumber.tsx
+++ src/components/sections/Home/OurNumber/OurNumber.tsx
@@ -6,7 +6,7 @@ import Label from './Label';
 
 export default function OurNumber() {
   return (
-    <section className="outnumber py-5 relative overflow-hidden">
+    <section className="outnumber relative overflow-hidden py-5">
       {/* Рядок рухається вліво */}
       <div className="animate-marquee-reverse decor relative inline-block w-[100%] whitespace-nowrap">
         <Image
@@ -24,7 +24,7 @@ export default function OurNumber() {
         <ul className="container z-10 grid h-[632px] grid-cols-2 xl:grid-cols-3">
           <li className="relative">
             <Label
-              className="translate-y-0 transform lg:translate-x-20 lg:translate-y-28 2xl:translate-x-36"
+              className="translate-y-0 transform lg:translate-x-14 lg:translate-y-10 xl:translate-x-36 xl:translate-y-24"
               textTop="учасників"
               textBottom="приймають участь в 2-3 проєктах"
               number={72}
@@ -33,7 +33,7 @@ export default function OurNumber() {
           </li>
           <li className="relative">
             <Label
-              className="translate-y-20 transform lg:translate-x-20 2xl:translate-x-52"
+              className="translate-x-[-97%] translate-y-96 transform lg:translate-x-20 lg:translate-y-32 xl:translate-y-16 xl:translate-x-48"
               textTop="учасників"
               textBottom="задоволені практикою і вважають цілі досягнуті"
               number={99}
@@ -42,7 +42,7 @@ export default function OurNumber() {
           </li>
           <li className="relative">
             <Label
-              className="transform xl:translate-x-16 xl:translate-y-52 2xl:translate-x-44"
+              className="-translate-y-24 translate-x-36 transform lg:translate-x-16 lg:translate-y-40 xl:translate-x-16 xl:translate-y-64 2xl:translate-x-44"
               textTop="анкет за 2024 рік"
               textBottom="від бажаючих прийняти участь в проєктах"
               number={2300}
@@ -51,8 +51,8 @@ export default function OurNumber() {
           </li>
           <li className="relative">
             <Label
-              className="-translate-x-1/3 translate-y-20 transform lg:translate-x-16"
-              textTop="жінок"
+              className="-translate-x-40 translate-y-[15%] transform lg:translate-x-[-6.65rem] xl:translate-x-16 xl:translate-y-20"
+              textTop="учасника"
               textBottom="працевлаштовані"
               number={102}
               showPercentage={false}
@@ -69,7 +69,7 @@ export default function OurNumber() {
           </li> */}
           <li className="relative">
             <Label
-              className="text- max-w-36 translate-x-40 translate-y-24 transform"
+              className="text- max-w-36 translate-x-40 translate-y-[-90%] transform lg:translate-x-[27rem] xl:translate-x-[10rem] xl:translate-y-28"
               textTop="кожен -й"
               textBottom="знаходіть роботу в обраному напрямку"
               number={4}
