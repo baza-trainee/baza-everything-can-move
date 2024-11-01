07e7e5cbabb9b00dfb9c2330c75d8b3e8cfca351
GannaKov <zlatta2000@gmail.com>
2024-11-01 18:37:23 +0100

    1 image 3d

--- src/components/sections/Home/ProjectsHomeSection/ParallaxCards.tsx
+++ src/components/sections/Home/ProjectsHomeSection/ParallaxCards.tsx
@@ -1,9 +1,71 @@
+'use client';
 import React from 'react';
+import Image from 'next/image';
+
+import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-card';
+import Link from 'next/link';
 
 // export interface ParallaxCardsProps {}
 
 const ParallaxCards: React.FC = () => {
-  return <div>ParallaxCards</div>;
+  //   const cards = [
+  //     {
+  //       id: 1,
+  //       depth: 5,
+  //       imageUrl: 'https://via.placeholder.com/300x200?text=Image+1',
+  //     },
+  //     {
+  //       id: 2,
+  //       depth: 10,
+  //       imageUrl: 'https://via.placeholder.com/300x200?text=Image+2',
+  //     },
+  //     {
+  //       id: 3,
+  //       depth: 15,
+  //       imageUrl: 'https://via.placeholder.com/300x200?text=Image+3',
+  //     },
+  //   ];
+
+  return (
+    <CardContainer>
+      <CardBody className="relative h-auto w-auto">
+        <CardItem translateZ="100" className="mt-4 w-full">
+          <Image
+            className="h-auto"
+            src="/assets/images/HomeProjects/organik.png"
+            alt="Logo Baza Trainee Ukraine"
+            priority
+            width="500"
+            height="500"
+            quality={100}
+            // className="h-60 w-full rounded-xl object-cover group-hover/card:shadow-xl"
+          />
+          ;
+        </CardItem>
+        <div className="mt-20 flex items-center justify-between">
+          {/* <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="rounded-xl px-4 py-2 text-xs font-normal dark:text-white"
+          >
+            Try now →
+          </CardItem> */}
+        </div>
+      </CardBody>
+    </CardContainer>
+  );
 };
 
 export default ParallaxCards;
+
+{
+  /* <Image
+  src="/assets/images/HomeProjects/organik.png"
+  alt="Logo Baza Trainee Ukraine"
+  priority
+  width={100}
+  height={100}
+/>; */
+}
--- src/components/sections/Home/ProjectsHomeSection/ProjectsHomeSection.tsx
+++ src/components/sections/Home/ProjectsHomeSection/ProjectsHomeSection.tsx
@@ -1,5 +1,7 @@
 import SectionTitle from '@/components/ui/SectionTitle';
 import React from 'react';
+import ParallaxCards from './ParallaxCards';
+import Image from 'next/image';
 
 // interface ProjectsHomeSectionProps {}
 
@@ -8,6 +10,7 @@ const ProjectsHomeSection: React.FC = () => {
     <section className="pb-[100px] lg:pb-20 2xl:pb-16">
       <div className="container">
         <SectionTitle className="mb-8"> проєкти бази</SectionTitle>
+        <ParallaxCards />
       </div>
     </section>
   );
