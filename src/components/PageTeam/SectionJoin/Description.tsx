34bb7b8d4c497925130a0f5dbae46018d7f4ea01
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-11-01 20:29:23 +0100

    fix &#39;

--- src/components/PageTeam/SectionJoin/Description.tsx
+++ src/components/PageTeam/SectionJoin/Description.tsx
@@ -1,10 +1,10 @@
 function Description() {
   return (
-    <div className="flex flex-col gap-6 text-s leading-[1.3] lg:max-w-[320px]  lg:text-m 2xl:max-w-[346px] 2xl:text-sm">
+    <div className="flex flex-col gap-6 text-s leading-[1.3] lg:max-w-[320px] lg:text-m 2xl:max-w-[346px] 2xl:text-sm">
       <p>
         Хочеш приєднатись до команди та створювати нові продукти? Є питання або
-        пропозиції? Відкритий до спонсорства? Звʼяжись з нами або відвідай наш
-        сайт.
+        пропозиції? Відкритий до спонсорства? Зв&#39;яжись з нами або відвідай
+        наш сайт.
       </p>
       <div className="flex flex-col gap-3">
         <a href="http://baza-trainee.tech">https://baza-trainee.tech</a>
