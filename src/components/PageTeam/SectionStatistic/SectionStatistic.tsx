1212c15bceb7bed1a91cf58378507f393aaef17d
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-11-02 23:41:33 +0100

    added MoveLine

--- src/components/PageTeam/SectionStatistic/SectionStatistic.tsx
+++ src/components/PageTeam/SectionStatistic/SectionStatistic.tsx
@@ -1,9 +1,16 @@
 import Label from './Label';
 import clsx from 'clsx';
+import MoveLine from '@/components/ui/MoveLine';
 
 function SectionStatistic() {
   return (
-    <section className="py-5">
+    <section className="relative py-5">
+      <MoveLine
+        className="absolute"
+        position="top"
+        direction="right"
+        duration={60}
+      />
       <div
         className={clsx(
           'relative bg-[url("/assets/images/TemSection/VectorMobile.svg")] bg-cover bg-center bg-no-repeat lg:bg-[url("/assets/images/TemSection/VectorTablet.svg")] 2xl:bg-[url("/assets/images/TemSection/VectorDesctop.svg")]'
@@ -62,6 +69,12 @@ function SectionStatistic() {
           </ul>
         </div>
       </div>
+      <MoveLine
+        className="absolute"
+        position="bottom"
+        direction="left"
+        duration={60}
+      />
     </section>
   );
 }
