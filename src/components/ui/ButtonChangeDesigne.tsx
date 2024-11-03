e7ecf8b4687c0a9470c428879c4663b0f6bcf07d
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-11-03 13:51:17 +0100

    Update ButtonChangeDesigne.tsx

--- src/components/ui/ButtonChangeDesigne.tsx
+++ src/components/ui/ButtonChangeDesigne.tsx
@@ -11,18 +11,18 @@ enum DesignType {
 
 function ButtonChangeDesigne() {
   const [isOpenButton, setIsOpenButton] = useState<boolean>(false);
-
-  const [typeDesine, setTypeDesigne] = useState<DesignType | ''>('');
+  const [typeDesine, setTypeDesigne] = useState<DesignType>(
+    (localStorage.getItem('designType') as DesignType) || DesignType.Olga
+  );
 
   useEffect(() => {
-    const storedDesignType =
-      (localStorage.getItem('designType') as DesignType) || '';
-    setTypeDesigne(storedDesignType);
-  }, []);
+    localStorage.setItem('designType', typeDesine);
+  }, [typeDesine]);
 
-  const setDisegn = (type: DesignType) => {
-    localStorage.setItem('designType', type);
-    setTypeDesigne(type);
+  const toggleDesign = () => {
+    setTypeDesigne((prev) =>
+      prev === DesignType.Olga ? DesignType.Svitlana : DesignType.Olga
+    );
   };
 
   return (
@@ -41,24 +41,25 @@ function ButtonChangeDesigne() {
       className="fixed bottom-5 left-5 rounded-full border-[1px] border-solid border-white bg-[#363535] p-[18px]"
     >
       <div className="relative flex items-center justify-start rounded-full">
+        {/* Перша кнопка */}
         <motion.button
-          onClick={() => setDisegn(DesignType.Olga)}
+          onClick={toggleDesign}
           initial={{ scale: 1 }}
           whileHover={{ scale: 1.2 }}
           transition={{ duration: 0.3 }}
           aria-label="Змінити дизайн"
           className={clsx(
             'h-6 w-6 rounded-full border-[2px] border-solid border-white',
-            typeDesine === DesignType.Svitlana
-              ? 'bg-[#8f8ded]'
-              : 'bg-olga-green'
+            typeDesine === DesignType.Olga ? 'bg-[#8f8ded]' : 'bg-olga-green'
           )}
         ></motion.button>
+
+        {/* Друга кнопка */}
         <AnimatePresence initial={false}>
           {isOpenButton && (
             <motion.button
               key="animated-button"
-              onClick={() => setDisegn(DesignType.Svitlana)}
+              onClick={toggleDesign}
               initial={{ opacity: 0, scale: 0, left: 0, right: 'auto' }}
               animate={{ opacity: 1, scale: 1, left: 'auto', right: 0 }}
               exit={{ opacity: 0, scale: 0.8 }}
@@ -67,7 +68,7 @@ function ButtonChangeDesigne() {
               aria-label="Змінити дизайн"
               className={clsx(
                 'absolute h-6 w-6 rounded-full border-[2px] border-solid border-white',
-                typeDesine === DesignType.Olga
+                typeDesine === DesignType.Svitlana
                   ? 'bg-[#8f8ded]'
                   : 'bg-olga-green'
               )}
