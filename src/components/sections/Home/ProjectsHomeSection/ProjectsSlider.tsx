9e2d82a144890297a6a45198de913dc503cb78b0
GannaKov <zlatta2000@gmail.com>
2024-11-07 09:01:23 +0100

    save

--- src/components/sections/Home/ProjectsHomeSection/ProjectsSlider.tsx
+++ src/components/sections/Home/ProjectsHomeSection/ProjectsSlider.tsx
@@ -27,7 +27,7 @@ export interface ProjectsSliderProps {
 const imageVariants = {
   enter: (direction: number) => {
     return {
-      x: direction > 0 ? '50%' : '-50%',
+      x: direction > 0 ? -1000 : 1000,
       scale: 0.5,
       opacity: 0.5,
       transition: {
@@ -49,7 +49,7 @@ const imageVariants = {
 
   exit: (direction: number) => {
     return {
-      x: direction > 0 ? '50%' : '-50%',
+      x: direction > 0 ? 1000 : -1000,
       scale: 0.5,
       opacity: 0.5,
       transition: {
@@ -62,7 +62,7 @@ const imageVariants = {
 
 const titleVariants = {
   enter: (direction: number) => {
-    return { x: direction > 0 ? '-10%' : '10%', opacity: 0.5 };
+    return { x: direction > 0 ? -1000 : 1000, opacity: 0 };
   },
   center: {
     x: 0,
@@ -73,21 +73,9 @@ const titleVariants = {
 
   exit: (direction: number) => {
     return {
-      x: direction > 0 ? '100%' : '-100%',
+      x: direction > 0 ? 1000 : -1000,
 
-      opacity: 0.5,
-    };
-  },
-  adjacent: (prevIndex: number, nextIndex: number) => {
-    return {
-      opacity: 0.7, // Слегка прозрачное
-
-      zIndex: 1, // На заднем плане
-      x: prevIndex && '-100%',
-      X: nextIndex && '100%',
-      transition: {
-        duration: 0.5,
-      },
+      opacity: 0,
     };
   },
 };
@@ -110,133 +98,69 @@ const ProjectsSlider: React.FC<ProjectsSliderProps> = ({ images }) => {
       prevIndex === 0 ? images.length - 1 : prevIndex - 1
     );
   };
-  // useEffect(() => {
-  //   if (isInView) {
-  //     const interval = setInterval(() => {
-  //       nextImage();
-  //     }, 15000);
-  //     return () => clearInterval(interval);
-  //   }
-  // }, [isInView, nextImage]);
+  useEffect(() => {
+    if (isInView) {
+      const interval = setInterval(() => {
+        nextImage();
+      }, 15000);
+      return () => clearInterval(interval);
+    }
+  }, [isInView, nextImage]);
 
   return (
     <div className="w-full lg:hidden" ref={ref}>
       {/* images div */}
-      <div
-        style={{ perspective: '1000px' }}
-        className="relative mb-6 flex h-[193px] w-full items-center justify-center overflow-hidden"
-      >
+      <div className="relative mb-6 flex w-full items-center justify-center overflow-hidden">
         <ICONS_SHARED.CORNER_BOTTOM className="absolute bottom-0 left-3" />
         <ICONS_SHARED.CORNER_BOTTOM className="absolute bottom-0 right-3 rotate-[270deg]" />
-
-        {images.map((image, index) => {
-          const pos = index - currentIndex;
-          const isCenter = pos === 0;
-          const nextIndex = (currentIndex + 1) % images.length;
-          const prevIndex = (currentIndex - 1 + images.length) % images.length;
-          const isAdjacent = index === nextIndex || index === prevIndex;
-          // console.log('currentIndex', currentIndex);
-          // console.log('nextIndex', nextIndex);
-          // console.log('prevIndex ', prevIndex);
-          return (
-            <>
-              {/* {isAdjacent && (
+        <AnimatePresence initial={false} custom={direction}>
+          {images.map(
+            (image, index) =>
+              index === currentIndex && (
                 <motion.div
-                  key={index}
-                  className="flex w-[120px]"
-                  custom={direction}
-                  variants={imageVariants}
-                  initial="enter"
-                  exit="exit"
-                  animate="adjacent"
-                  style={{
-                    transformStyle: 'preserve-3d',
-                  }}
+                  key={image.pos}
+                  className="shadow-olga-combined mx-auto flex w-[320px] flex-col items-center"
                 >
-                  <Image
-                    src={image.src}
-                    className="object-cover"
-                    alt={image.name}
-                    style={{
-                      width: '100%',
-                      height: '100%',
-                      transform: 'translateZ(10px)',
-                      zIndex: 0,
+                  <motion.div
+                    custom={direction}
+                    variants={imageVariants}
+                    initial="enter"
+                    animate={['center', 'expand']}
+                    exit="exit"
+                    className="mb-2 flex h-[164px] w-full items-center justify-center overflow-hidden"
+                  >
+                    <Image
+                      src={image.src}
+                      className="object-cover"
+                      alt={image.name}
+                      style={{ width: '100%', height: '100%' }}
+                    />
+                  </motion.div>
+
+                  {/* title */}
+
+                  <motion.div
+                    className="relative w-full"
+                    custom={direction}
+                    variants={titleVariants}
+                    initial="enter"
+                    animate={['center']}
+                    exit="exit"
+                    transition={{
+                      x: { type: 'spring', stiffness: 300, damping: 30 },
+                      opacity: { duration: 0.2 },
                     }}
-                  />
+                  >
+                    <h3 className="text-center text-s leading-o-150 text-olga-light-grey">
+                      {image.name}
+                    </h3>
+                  </motion.div>
+
+                  {/* </div> */}
                 </motion.div>
-              )} */}
-              {/* norm!!!!! */}
-
-              <motion.div
-                className="shadow-olga-combined mx-auto flex w-[320px] flex-col items-center"
-                style={{
-                  transformStyle: 'preserve-3d',
-                }}
-                initial={{
-                  opacity: 0,
-                  y: 20,
-                }}
-                animate={{
-                  opacity: 1,
-                  y: 0,
-                  transition: {
-                    duration: 0.5,
-                    delay: 0.2 * index,
-                    ease: 'easeOut',
-                    once: true,
-                  },
-                }}
-                key={'card' + index}
-              >
-                {/* <motion.div
-                  custom={direction}
-                  variants={imageVariants}
-                  initial="enter"
-                  animate={['center', 'expand']}
-                  exit="exit"
-                  className="mb-2 flex h-[164px] w-full items-center justify-center overflow-hidden"
-                  style={{
-                    transformStyle: 'preserve-3d',
-                  }}
-                > */}
-                <Image
-                  src={image.src}
-                  className="object-cover"
-                  alt={image.name}
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    transform: 'translateZ(100px)',
-                  }}
-                />
-                {/* </motion.div> */}
-
-                {/* title */}
-
-                {/* <motion.div
-                  className="relative w-full"
-                  custom={direction}
-                  variants={titleVariants}
-                  initial="enter"
-                  animate={['center']}
-                  exit="exit"
-                  transition={{
-                    x: { type: 'spring', stiffness: 300, damping: 30 },
-                    opacity: { duration: 0.2 },
-                  }}
-                >
-                  <h3 className="text-center text-s leading-o-150 text-olga-light-grey">
-                    {image.name}
-                  </h3>
-                </motion.div> */}
-
-                {/* </div> */}
-              </motion.div>
-            </>
-          );
-        })}
-
+              )
+          )}
+        </AnimatePresence>
         {/*project card */}
       </div>
       {/* buttons div */}
