63618602fa7cb8b475af9b01f6e6b8bc8c4e2adf
GannaKov <zlatta2000@gmail.com>
2024-11-08 03:29:57 +0100

    clean

--- src/components/sections/Home/ProjectsHomeSection/PCardsThird.tsx
+++ src/components/sections/Home/ProjectsHomeSection/PCardsThird.tsx
@@ -4,9 +4,7 @@ import React from 'react';
 import Image from 'next/image';
 import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-third';
 import { FollowerPointerCard } from '@/components/ui/FollowerPointerCard';
-
 import { IMAGES_HOME_PROJECTS } from '@/constants/images/imagesSrc';
-import ProjectsSlider from './ProjectsSlider';
 import ProjectsSlider3 from './ProjectsSlider3';
 
 export function PCardsThird() {
@@ -75,7 +73,7 @@ export function PCardsThird() {
         </FollowerPointerCard>
       </div>
       {/* mobile */}
-      <ProjectsSlider images={IMAGES_HOME_PROJECTS} />
+
       <ProjectsSlider3 images={IMAGES_HOME_PROJECTS} />
     </>
   );
