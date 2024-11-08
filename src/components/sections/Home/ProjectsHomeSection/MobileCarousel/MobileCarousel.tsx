3d2fc449d99b42900f89d69cdea49c2d638604fc
GannaKov <zlatta2000@gmail.com>
2024-11-09 00:30:19 +0100

    save

--- src/components/sections/Home/ProjectsHomeSection/MobileCarousel/MobileCarousel.tsx
+++ src/components/sections/Home/ProjectsHomeSection/MobileCarousel/MobileCarousel.tsx
@@ -11,60 +11,76 @@ interface ProjectsSliderProps {
 }
 
 const MobileCarousel: React.FC<ProjectsSliderProps> = ({ images }) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const isInView = useInView(containerRef);
   const [currentIndex, setCurrentIndex] = useState(0);
+
   const totalImages = images.length;
 
   console.log('currentIndex', currentIndex);
 
-  const handleNext = () => {
+  const handleNext = useCallback(() => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % totalImages);
-  };
+  }, [totalImages]);
 
   const handleBack = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + totalImages) % totalImages);
   };
 
-  //   (prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1)
+  // useEffect(() => {
+  //   if (isInView) {
+  //     const interval = setInterval(() => {
+  //       handleNext();
+  //     }, 5000);
+  //     return () => clearInterval(interval);
+  //   }
+  // }, [isInView, handleNext]);
 
   const positions = ['right', 'center', 'left'];
 
   const imageVariants = {
     center: { x: '0%', scale: 1, zIndex: 5 },
-
     left: { x: '-100%', scale: 0.5, zIndex: 2 },
     right: { x: '100%', scale: 0.5, zIndex: 2 },
   };
   const getPositionIndex = (baseIndex: number, offset: number) => {
     return (baseIndex + offset + totalImages) % totalImages;
   };
-  // const isPrevious =
-  //   index === (currentIndex - 1 + images.length) % images.length;
-  // const isNext = index === (currentIndex + 1) % images.length;
-  return (
-    <div className="flex h-[320px] flex-col items-center justify-center">
-      {positions.map((position, posIndex) => {
-        const imageIndex = getPositionIndex(currentIndex, posIndex - 1);
-        return (
-          <motion.img
-            key={imageIndex}
-            src={
-              typeof images[imageIndex].src === 'string'
-                ? images[imageIndex].src
-                : images[imageIndex].src.src
-            }
-            alt={`Image ${imageIndex}`}
-            className="rounded-[12px]"
-            initial="left"
-            animate={position}
-            variants={imageVariants}
-            transition={{ duration: 0.5 }}
-            style={{ width: '40%', position: 'absolute' }}
-          />
-        );
-      })}
 
+  return (
+    <div
+      className="w-full lg:hidden"
+      style={{ perspective: '1000px' }}
+      ref={containerRef}
+    >
+      <div className="relative mb-6 flex h-[193px] w-[343px] items-center justify-center overflow-hidden">
+        <ICONS_SHARED.CORNER_BOTTOM className="absolute bottom-0 left-3" />
+        <ICONS_SHARED.CORNER_BOTTOM className="absolute bottom-0 right-3 rotate-[270deg]" />
+        {/* <div className="flex h-[320px] flex-col items-center justify-center"> */}
+        {positions.map((position, posIndex) => {
+          const imageIndex = getPositionIndex(currentIndex, posIndex - 1);
+          return (
+            <motion.img
+              key={imageIndex}
+              src={
+                typeof images[imageIndex].src === 'string'
+                  ? images[imageIndex].src
+                  : images[imageIndex].src.src
+              }
+              alt={`Image ${imageIndex}`}
+              className="rounded-[12px]"
+              initial="left"
+              animate={position}
+              variants={imageVariants}
+              transition={{ duration: 0.5 }}
+              style={{ width: '40%', position: 'absolute' }}
+            />
+          );
+        })}
+      </div>
       {/* buttons div */}
-      {/* <div className="flex justify-center gap-5">
+      <div className="flex justify-center gap-5">
         <ButtonSlide onClick={handleBack} ariaLabel="кнопка попереднє фото" />
 
         <ButtonSlide
@@ -72,20 +88,6 @@ const MobileCarousel: React.FC<ProjectsSliderProps> = ({ images }) => {
           ariaLabel="кнопка наступне фото"
           onClick={handleNext}
         />
-      </div> */}
-      <div className="flex flex-row gap-3">
-        <button
-          className="bg-indigo-400 mt-[400px] rounded-md px-4 py-2 text-white"
-          onClick={handleBack}
-        >
-          Back
-        </button>
-        <button
-          className="bg-indigo-400 mt-[400px] rounded-md px-4 py-2 text-white"
-          onClick={handleNext}
-        >
-          Next
-        </button>
       </div>
     </div>
   );
