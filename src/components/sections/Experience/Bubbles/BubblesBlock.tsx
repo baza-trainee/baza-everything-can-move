f72c2588f3cd42a4141908be5a8114e830760006
Viktoria Vonsovych <vikvons@gmail.com>
2024-11-09 23:49:21 +0100

    experience

--- src/components/sections/Experience/Bubbles/BubblesBlock.tsx
+++ src/components/sections/Experience/Bubbles/BubblesBlock.tsx
@@ -5,25 +5,17 @@ import Bubbles3D from './Bubbles3D'
 import Bubbles2D from './Bubbles2D'
 
 
-
-
 const BubblesBlock:React.FC = () => {
 
-  // useEffect(() =>{
-
-  // },[])
-
-   const bubblesBlockRef = useRef<HTMLDivElement | null >(null);
-  // let params = bubblesBlockRef.current?.getBoundingClientRect();
-
-  // console.log(params.width, params.height);
+  const bubblesBlockRef = useRef<HTMLDivElement | null >(null);
 
   return (
-    <div ref={bubblesBlockRef} className='relative'>
-      <Bubbles3D/>
-      <Bubbles2D/>
-    </div>
-  )
+      <div ref={bubblesBlockRef} className='relative pb-16'>
+        <Bubbles3D/>
+        <Bubbles2D/>
+      </div>
+   )
+   
 }
 
 export default BubblesBlock
