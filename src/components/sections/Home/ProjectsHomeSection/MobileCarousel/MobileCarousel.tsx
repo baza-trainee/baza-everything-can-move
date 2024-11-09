98b69c57ddcc206dfc2d4aac4be0f636620f612a
GannaKov <zlatta2000@gmail.com>
2024-11-09 03:23:15 +0100

    fix direction!

--- src/components/sections/Home/ProjectsHomeSection/MobileCarousel/MobileCarousel.tsx
+++ src/components/sections/Home/ProjectsHomeSection/MobileCarousel/MobileCarousel.tsx
@@ -16,7 +16,7 @@ const MobileCarousel: React.FC<ProjectsSliderProps> = ({ images }) => {
   const isInView = useInView(containerRef);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [direction, setDirection] = useState(0);
-
+  console.log(currentIndex);
   const totalImages = images.length;
 
   const handleNext = useCallback(() => {
@@ -39,11 +39,22 @@ const MobileCarousel: React.FC<ProjectsSliderProps> = ({ images }) => {
   // }, [isInView, handleNext]);
 
   const positions = ['right', 'center', 'left'];
-
+  console.log(direction);
   const imageVariants = {
     center: { x: '0%', scale: 1, zIndex: 5 },
-    left: { x: '-50%', scale: 0.4, zIndex: 2 },
-    right: { x: '50%', scale: 0.4, zIndex: 2 },
+
+    left: (direction: number) => ({
+      //  x: direction > 0 ? '-50%' : '50%',
+      x: '-50%',
+      scale: 0.4,
+      zIndex: 2,
+    }),
+    right: (direction: number) => ({
+      // x: direction > 0 ? '50%' : '-50%',
+      x: '50%' ,
+      scale: 0.4,
+      zIndex: 2,
+    }),
   };
   const titleVariants = {
     exit: (direction: number) => ({
@@ -73,42 +84,44 @@ const MobileCarousel: React.FC<ProjectsSliderProps> = ({ images }) => {
         <ICONS_SHARED.CORNER_BOTTOM className="absolute bottom-0 right-3 rotate-[270deg]" />
 
         <AnimatePresence initial={false} custom={direction}>
+          {/* {posIndex === 1 && ( */}
+          <motion.div
+            key={`title-${currentIndex}`}
+            className="absolute bottom-0 w-full"
+            custom={direction}
+            variants={titleVariants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{ duration: 0.5 }}
+          >
+            <h3 className="text-center text-s leading-o-150 text-olga-light-grey">
+              {images[currentIndex].name}
+            </h3>
+          </motion.div>
+          {/* )} */}
           {positions.map((position, posIndex) => {
             const imageIndex = getPositionIndex(currentIndex, posIndex - 1);
             return (
-              <React.Fragment key={`fragment-${posIndex}`}>
-                <motion.div
-                  key={`image-${imageIndex}`}
-                  initial="left"
-                  animate={position}
-                  variants={imageVariants}
-                  transition={{ duration: 0.7 }}
-                  className="absolute top-0 h-[164px] w-[320px]"
-                >
-                  <Image
-                    src={images[imageIndex].src}
-                    alt={`Image ${imageIndex}`}
-                    className="object-cover"
-                    style={{ width: '100%' }}
-                  />
-                </motion.div>
-                {posIndex === 1 && (
-                  <motion.div
-                    key={`title-${currentIndex}`}
-                    className="absolute bottom-0 w-full"
-                    custom={direction}
-                    variants={titleVariants}
-                    initial="enter"
-                    animate="center"
-                    exit="exit"
-                    transition={{ duration: 0.5 }}
-                  >
-                    <h3 className="text-center text-s leading-o-150 text-olga-light-grey">
-                      {images[currentIndex].name}
-                    </h3>
-                  </motion.div>
-                )}
-              </React.Fragment>
+              // <React.Fragment key={`fragment-${posIndex}`}>
+              <motion.div
+                key={`image-${imageIndex}`}
+                custom={direction}
+                initial="left"
+                animate={position}
+                variants={imageVariants}
+                transition={{ duration: 0.7 }}
+                className="absolute top-0 h-[164px] w-[320px]"
+              >
+                <Image
+                  src={images[imageIndex].src}
+                  alt={`Image ${imageIndex}`}
+                  className="object-cover"
+                  style={{ width: '100%' }}
+                />
+              </motion.div>
+
+              // </React.Fragment>
             );
           })}
         </AnimatePresence>
