36d5d6122657b82f55a48457be5a85f15c7703c9
Viktoria Vonsovych <vikvons@gmail.com>
2024-11-10 16:49:24 +0100

    experience

--- src/components/sections/Experience/Bubbles/BubblesBlock.tsx
+++ src/components/sections/Experience/Bubbles/BubblesBlock.tsx
@@ -3,14 +3,23 @@
 import React, {useRef} from 'react'
 import Bubbles3D from './Bubbles3D'
 import Bubbles2D from './Bubbles2D'
+import SubTitle from '@/components/ui/SubTitle';
+import SectionTitle from '@/components/ui/SectionTitle';
+
+export interface BubblesSectionProps {
+  title: string,
+  subtitle: string
+}
 
 
-const BubblesBlock:React.FC = () => {
+const BubblesBlock = ({title, subtitle}:BubblesSectionProps) => {
 
   const bubblesBlockRef = useRef<HTMLDivElement | null >(null);
 
   return (
       <div ref={bubblesBlockRef} className='relative pb-16'>
+        <SectionTitle>{title}</SectionTitle>
+        <SubTitle>{subtitle}</SubTitle>
         <Bubbles3D/>
         <Bubbles2D/>
       </div>
