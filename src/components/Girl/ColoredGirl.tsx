e28c35ae80557ed0cef691bb6fdb30e2a1e57c4e
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-11-11 20:45:37 +0200

    changed the animation for the running line

--- src/components/Girl/ColoredGirl.tsx
+++ src/components/Girl/ColoredGirl.tsx
@@ -40,7 +40,7 @@ function GirlModel() {
 export default function ColoredGirl() {
   return (
     <>
-      <div className="h-[700px] 2xl:w-[50%] lg:w-[60%] w-[90%] transform 2xl:translate-x-[70%] lg:translate-x-[55%] 2xl:translate-y-[-25%] translate-y-[-7%] translate-x-[-10%] z-10 absolute">
+      <div className="h-[700px] 2xl:w-[50%] lg:w-[60%] w-[90%] transform 2xl:translate-x-[70%] lg:translate-x-[55%] 2xl:translate-y-[-25%] translate-y-[-7%] translate-x-[-10%] absolute">
         {/* <h1 className="text-white text-center text-lg">I hate her !!!</h1> */}
         <Canvas className="h-[900px] w-[90%] z-10"
           camera={{ position: [0, 3, 5], fov: 61 }}
