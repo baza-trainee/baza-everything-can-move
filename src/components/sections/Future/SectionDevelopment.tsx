fbb3268807a427c5c10ed126e89855cebcee03f1
bohdanalu <b_dana@ukr.net>
2024-11-12 11:16:10 +0200

    update animation in text

--- src/components/sections/Future/SectionDevelopment.tsx
+++ src/components/sections/Future/SectionDevelopment.tsx
@@ -6,65 +6,22 @@ import React, { useRef, useEffect, useState } from 'react';
 import DevelopmentText from './TextSections/DevelopmentText';
 import BrainAnimation from './AnimatedSections/BrainAnimation';
 
+
+
 function SectionDevelopment() {
   const sectionRef = useRef<HTMLDivElement>(null);
-  const [isScrollingInChild, setIsScrollingInChild] = useState(false);
-  const [isFullyVisible, setIsFullyVisible] = useState(false);
-
-  // Check for section full visibility
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => setIsFullyVisible(entry.isIntersecting),
-      { threshold: 1.0 }
-    );
-
-    if (sectionRef.current) {
-      observer.observe(sectionRef.current);
-    }
-
-    return () => {
-      if (sectionRef.current) {
-        observer.unobserve(sectionRef.current);
-      }
-    };
-  }, []);
-
-  // Handle scroll lock based on visibility and scrolling status
-  useEffect(() => {
-    if (isFullyVisible && isScrollingInChild) {
-      // Lock scroll on the parent and document
-      if (sectionRef.current) {
-        sectionRef.current.style.overflow = 'hidden';
-      }
-      document.body.style.overflow = 'hidden';
-    } else {
-      // Unlock scroll
-      if (sectionRef.current) {
-        sectionRef.current.style.overflow = '';
-      }
-      document.body.style.overflow = '';
-    }
-  }, [isFullyVisible, isScrollingInChild]);
-
-  // Handle child scroll state updates
-  const handleChildScrollUpdate = (scrollInfo: { isTop: boolean; isBottom: boolean }) => {
-    setIsScrollingInChild(!(scrollInfo.isTop || scrollInfo.isBottom));
-  };
 
   return (
-    <section>
+    <section ref={sectionRef}>
       <div className="container">
-        <ContainerWithCorners className="mb-[100px] lg:mb-[80px] xl:mb-[64px]">
+        <ContainerWithCorners className="mb-[100px] lg:mb-[80px] 2xl:mb-[64px]">
           <SectionTitle>Розвиток</SectionTitle>
           <SubTitle>Розвивайся з Базою</SubTitle>
-          <div
-            ref={sectionRef}
-            className="flex flex-col justify-items-center py-[32px] lg:flex-row lg:items-center"
-          >
+          <div className="scrollbar-hidden flex h-full max-h-full flex-col justify-items-center py-[32px] lg:flex-row lg:items-center">
             <div className="grow">
-              <BrainAnimation />{' '}
+              <BrainAnimation />
             </div>
-            <DevelopmentText onScrollUpdate={handleChildScrollUpdate} />
+            <DevelopmentText />
           </div>
         </ContainerWithCorners>
       </div>
@@ -72,4 +29,11 @@ function SectionDevelopment() {
   );
 }
 
+
+
+
 export default SectionDevelopment;
+
+
+
+
--- src/components/sections/Future/TextSections/DevelopmentText.tsx
+++ src/components/sections/Future/TextSections/DevelopmentText.tsx
@@ -1,75 +1,143 @@
 'use client';
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { TEXTS } from '../../../../constants/text';
 
-interface DevelopmentTextProps {
-  onScrollUpdate?: (scrollInfo: { isTop: boolean; isBottom: boolean }) => void;
-}
+// interface DevelopmentTextProps {
+//   onScrollUpdate?: (scrollInfo: { isTop: boolean; isBottom: boolean }) => void;
+// }
 
-const DevelopmentText: React.FC<DevelopmentTextProps> = ({ onScrollUpdate }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const scrollTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+// const DevelopmentText: React.FC<DevelopmentTextProps> = ({ onScrollUpdate }) => {
+//   const containerRef = useRef<HTMLDivElement>(null);
 
-  // Debounce function to limit the scroll updates
-  const debounceScrollUpdate = (callback: () => void, delay: number) => {
-    if (scrollTimeoutRef.current) {
-      clearTimeout(scrollTimeoutRef.current);
-    }
-    scrollTimeoutRef.current = setTimeout(callback, delay);
-  };
+//   const handleScroll = () => {
+//     if (!containerRef.current) return;
 
-  const handleScroll = () => {
-    if (!containerRef.current) return;
+//     const container = containerRef.current;
+//     const isTop = container.scrollTop === 0;
+//     const isBottom = container.scrollTop + container.clientHeight >= container.scrollHeight;
 
-    const container = containerRef.current;
-    const isTop = container.scrollTop === 0;
-    const isBottom = container.scrollTop + container.clientHeight >= container.scrollHeight;
+//     // Call onScrollUpdate to notify the parent about the scroll position
+//     onScrollUpdate?.({ isTop, isBottom });
+//   };
 
-    // Use debounce function to limit the number of calls to onScrollUpdate
-    debounceScrollUpdate(() => {
-      onScrollUpdate?.({ isTop, isBottom });
-    }, 100); // 100ms delay
-  };
+//   useEffect(() => {
+//     const container = containerRef.current;
+//     container?.addEventListener('scroll', handleScroll);
+
+//     return () => {
+//       container?.removeEventListener('scroll', handleScroll);
+//     };
+//   }, []);
+
+//   return (
+//     <div className="mt-8 h-[340px] lg:mt-0 lg:w-[320px] 2xl:h-[376px]">
+//       <div
+//         ref={containerRef}
+//         style={{
+//           overflowY: 'scroll',
+//           scrollbarWidth: 'none', // Firefox
+//           msOverflowStyle: 'none', // IE
+//           WebkitOverflowScrolling: 'touch', // Smooth scroll on mobile
+//         }}
+//         className="scrollbar-hidden flex h-full max-h-full flex-col justify-items-center overflow-y-scroll"
+//       >
+//         {TEXTS.map((item, index) => (
+//           <div key={index} className="h-full flex-shrink-0">
+//             <h3 className="mb-[20px] text-sm leading-o-130 lg:mb-[29px] lg:text-md lg:uppercase 2xl:text-l">
+//               {item.title}
+//             </h3>
+//             <p className="text-s leading-o-130 lg:text-m 2xl:text-sm">
+//               {item.text}
+//             </p>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+import { motion, AnimatePresence } from 'framer-motion';
+
+const DevelopmentText: React.FC = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    const container = containerRef.current;
-    container?.addEventListener('scroll', handleScroll);
-
-    return () => {
-      container?.removeEventListener('scroll', handleScroll);
-
-      // Clean up the timeout on unmount
-      if (scrollTimeoutRef.current) {
-        clearTimeout(scrollTimeoutRef.current);
-      }
-    };
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % TEXTS.length);
+    }, 6000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="mt-8 h-[340px] lg:mt-0 lg:w-[320px] xl:h-[376px] xl:w-[346px]">
-      <div
-        ref={containerRef}
-        style={{
-          overflowY: 'scroll',
-          scrollbarWidth: 'none', // Firefox
-          msOverflowStyle: 'none', // IE
-          WebkitOverflowScrolling: 'touch', // Smooth scroll on mobile
-        }}
-        className="scrollbar-hidden flex h-full max-h-full flex-col overflow-y-scroll"
-      >
-        {TEXTS.map((item, index) => (
-          <div key={index} className="h-full flex-shrink-0">
-            <h3 className="mb-[20px] text-sm leading-o-130 lg:mb-[29px] lg:text-md lg:uppercase xl:text-l">
-              {item.title}
-            </h3>
-            <p className="text-s leading-o-130 lg:text-m xl:text-sm">
-              {item.text}
-            </p>
-          </div>
-        ))}
+    <div className="relative mt-8 h-[120px] overflow-hidden lg:mt-0 lg:h-[340px] lg:w-[320px] 2xl:h-[376px]">
+      <div className="relative h-full overflow-hidden">
+        <AnimatePresence>
+          {TEXTS.map(
+            (item, index) =>
+              index === currentIndex && (
+                <motion.div
+                  key={index}
+                  initial={{ y: '100%', opacity: 0 }} // Початкова позиція з 0 прозорістю
+                  animate={{ y: 0, opacity: 1 }}
+                  exit={{ y: '-100%', opacity: 0 }}
+                  transition={{
+                    y: { duration: 4, ease: 'easeInOut' },
+                    opacity: { duration: 4.5 },
+                    delay: index === currentIndex ? 2.5 : 0,
+                  }}
+                  className="absolute flex h-full w-full lg:items-center 2xl:items-start"
+                >
+                  <div>
+                    <h3 className="xl:text-l mb-[20px] text-sm leading-o-130 lg:mb-[29px] lg:text-md lg:uppercase">
+                      {item.title}
+                    </h3>
+                    <p className="xl:text-sm text-s leading-o-130 lg:text-m">
+                      {item.text}
+                    </p>
+                  </div>
+                </motion.div>
+              )
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
 };
+
+// const marqueeAnimation = {
+//   y: ["0%", "-100%"],
+//   transition: {
+//     duration: 15,
+//     ease: "linear",
+//     repeat: Infinity,
+//   },
+// };
+
+// return (
+//   <div className="mt-8 h-[340px] lg:mt-0 lg:w-[320px] 2xl:h-[376px] overflow-hidden relative">
+//     <div className="overflow-y-hidden h-full">
+//       <motion.div animate={marqueeAnimation}>
+//         {TEXTS.map((item, index) => (
+//           <motion.div
+//             className="h-[340px]"
+//             key={index}
+//             initial={{ opacity: 1 }}
+//             animate={{ opacity: [1, 0] }} // Прозорість кожного блоку поступово зменшується
+//             transition={{
+//               duration: 3, // Тривалість зникнення
+//               delay: index * 3, // Затримка для кожного блоку
+//               repeat: Infinity,
+//               repeatDelay: (TEXTS.length - 1) * 3, // Час між циклами
+//             }}
+//           >
+//             <div>
+//               <h3>{item.title}</h3>
+//               <p>{item.text}</p>
+//             </div>
+//           </motion.div>
+//         ))}
+//       </motion.div>
+//     </div>
+//   </div>
+// );
 
 export default DevelopmentText;
