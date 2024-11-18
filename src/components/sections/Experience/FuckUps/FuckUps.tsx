c8bccefd926d63dfbb0931025a19a36bac3512b0
Viktoria Vonsovych <vikvons@gmail.com>
2024-11-18 23:07:13 +0100

    experience

--- src/components/sections/Experience/FuckUps/FuckUps.tsx
+++ src/components/sections/Experience/FuckUps/FuckUps.tsx
@@ -13,17 +13,14 @@ const FuckUpSection = ({title, subtitle}:FuckUpsSectionProps) => {
   return (
     <div className='relative'>
       
-
-      <div className='container pb-8'>
+      <div className='container pb-8 sticky'>
       <ICONS_SHARED.CORNER_TOP className="float-right" />
           <SectionTitle>{title}</SectionTitle>
           <SubTitle>{subtitle}</SubTitle>
       </div>
       <WiresAnimationComponent />
 
-      <div className="container"><ICONS_SHARED.CORNER_BOTTOM className="float-left" /></div>
-
-      {/* <p className='uppercase text-l leading-[31.2px] font-regular bg-olga-btn-menu py-10 px-[124px] my-16'>Baza Trainee UKRAINE це синергія ініціативності та ентузіазму. Віримо, що найм джуніорів — це не лише інвестиція в майбутнє компанії, а й можливість ростити власних професіоналів з індивідуальним підходом та свіжим баченням.</p> */}
+      <p className='lg:uppercase text-sm 2xl:text-l leading-[23.4px] 2xl:leading-[31.2px] font-regular bg-olga-btn-menu py-10 lg:py-8 2xl:py-10 px-4 lg:px-6 2xl:px-[124px] my-16'>Baza Trainee UKRAINE це синергія ініціативності та ентузіазму. Віримо, що найм джуніорів — це не лише інвестиція в майбутнє компанії, а й можливість ростити власних професіоналів з індивідуальним підходом та свіжим баченням.</p>
     </div>
   )
 }
