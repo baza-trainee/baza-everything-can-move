9fc1f2d8ea99d118d532de10a87e89c0b0850b64
GannaKov <zlatta2000@gmail.com>
2024-11-20 18:27:28 +0100

    title mobile

--- src/components/sections/Projects/DesignBySvitlana/BirdsComponent.tsx
+++ src/components/sections/Projects/DesignBySvitlana/BirdsComponent.tsx
@@ -1,9 +1,19 @@
+import SectionTitle from '@/components/ui/SectionTitle';
 import BirdsScene from './Birds/BirdsScene';
 
 const BirdsComponent = () => {
   return (
     //md:w-[375px] lg:w-[768px] 2xl:w-[1440px]
-    <section className="m-auto mt-[57px] h-[749px] w-full overflow-hidden lg:mt-[18px] lg:h-[906px] 2xl:mt-0 2xl:h-[749px]">
+    <section className="relative m-auto mt-[57px] h-[749px] w-full overflow-hidden lg:mt-[18px] lg:h-[906px] 2xl:mt-0 2xl:h-[749px]">
+      <div className="absolute left-[50%] top-[266px] translate-x-[-50%] text-center">
+        <SectionTitle
+          secondDesign
+        //   className="absolute left-[50%] top-[266px] translate-x-[-50%]"
+        >
+          Наші проєкти
+        </SectionTitle>
+      </div>
+
       <BirdsScene />
     </section>
   );
