c2e0baec0e0b23fa54c7e8003feebf91f81c4ede
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-11-24 21:48:18 +0100

    add scroll team section

--- src/components/sections/PageTeam/desingeBySvitlana/TeamSection/components/ui/horizontalVariants.ts
+++ src/components/sections/PageTeam/desingeBySvitlana/TeamSection/components/ui/horizontalVariants.ts
@@ -1,50 +1,50 @@
 import { Variants } from 'framer-motion';
+interface VariantsProps {
+  length: number;
+  elementWidth?: number;
+  gap?: number;
+}
 
-export const generateVariantsHorizontal = (length: number) => {
+export const generateVariantsHorizontal = ({
+  length,
+  elementWidth,
+  gap,
+}: VariantsProps) => {
   const variants: Variants = {};
   const centerIndex = Math.floor(length / 2);
-  const spacing = 100 / (length + 1);
+  const spacing = 14;
 
   for (let i = 0; i < length; i++) {
-    const leftPosition = spacing * (i + 1);
-
-    if (i <= centerIndex - 2) {
+    if (i < centerIndex) {
       variants[`left${centerIndex - i}`] = {
-        left: `${leftPosition}%`,
-        scale: 0.8,
-      };
-    } else if (i === centerIndex - 1) {
-      variants[`left${centerIndex - i}`] = {
-        left: `10%`,
-        scale: 0.8,
+        left: `${50 - spacing * (centerIndex - i)}%`, //50-14 * 4-0 // 50 - 14 * 4 -
+        translateX: '-50%',
+        scale: 0.57,
       };
     } else if (i === centerIndex) {
       variants.center = {
         left: '50%',
-        // translateX: '-50%',
+        translateX: '-50%',
         scale: 1,
-      };
-    } else if (i === centerIndex + 1) {
-      variants[`right${i - centerIndex}`] = {
-        left: '90%',
-        scale: 0.8,
       };
     } else {
       variants[`right${i - centerIndex}`] = {
-        left: `${leftPosition}%`,
-        scale: 0.8,
+        left: `${50 + spacing * (i - centerIndex)}%`,
+        translateX: '-50%',
+        scale: 0.57,
       };
     }
   }
 
   return variants;
 };
+
 export const generatePositions = (length: number) => {
   const positions = [];
   const centerIndex = Math.floor(length / 2);
 
   for (let i = 0; i < length; i++) {
-    if (i <= centerIndex - 1) {
+    if (i < centerIndex) {
       positions.push(`left${centerIndex - i}`);
     } else if (i === centerIndex) {
       positions.push('center');
