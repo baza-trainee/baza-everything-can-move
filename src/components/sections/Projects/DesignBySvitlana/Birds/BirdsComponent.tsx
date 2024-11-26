b32737197ea529acd26adcf79accc88d39d31486
GannaKov <zlatta2000@gmail.com>
2024-11-26 20:20:06 +0100

    plashka

--- src/components/sections/Projects/DesignBySvitlana/Birds/BirdsComponent.tsx
+++ src/components/sections/Projects/DesignBySvitlana/Birds/BirdsComponent.tsx
@@ -4,20 +4,25 @@ import BirdsScene from './BirdsScene';
 const BirdsComponent = () => {
   return (
     //md:w-[375px] lg:w-[768px] 2xl:w-[1440px]
-    <section className="relative m-auto h-[803px] w-full overflow-hidden pt-[54px] lg:h-[924px] lg:pt-[18px] 2xl:mt-0 2xl:h-[754px]">
+    <section className="relative m-auto h-[803px] w-full overflow-hidden lg:h-[924px] 2xl:mt-0 2xl:h-[754px]">
       <div className="absolute left-[50%] top-[254px] w-[300px] translate-x-[-50%] text-center lg:top-[238px] lg:w-[409px] 2xl:left-80 2xl:top-[132px] 2xl:w-[450px] 2xl:text-left">
         <SectionTitle secondDesign className="mb-6">
           <span className="text-white">Наші</span>
           <br />
           <span>проєкти</span>
         </SectionTitle>
-        <p className="2xl:leading-s-36 font-third-family text-m font-regular leading-s-24 2xl:text-l">
+        <p className="font-third-family text-m font-regular leading-s-24 2xl:text-l 2xl:leading-s-36">
           Усі проєкти створюються українцями, які опановують ІТ-спеціальності
           незалежно від віку та досвіду — серед них є трейні, джуніори й
           світчери.
         </p>
       </div>
-
+      <div
+        className="absolute top-0 h-20 w-full"
+        style={{
+          background: ` linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.5) 57.08%, rgba(0, 0, 0, 0.257173) 80.1%, rgba(0, 0, 0, 0) 98.1%)`,
+        }}
+      ></div>
       <BirdsScene />
       <div
         className="absolute bottom-0 h-20 w-full"
--- src/components/sections/Projects/DesignBySvitlana/ProjectsSection/SliderDesktop.tsx
+++ src/components/sections/Projects/DesignBySvitlana/ProjectsSection/SliderDesktop.tsx
@@ -1,8 +1,10 @@
 'use client';
 import React from 'react';
+import Image from 'next/image';
 import { ProjectsImagesProps } from './types';
 import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-third';
-import ProjectCard from './ProjectCard';
+
+import StarsBtnComponent from '@/components/ui/DesignBySvitlna/StarsBtnComponent';
 
 const SliderDesktop: React.FC<ProjectsImagesProps> = ({ images }) => {
   return (
@@ -19,12 +21,36 @@ const SliderDesktop: React.FC<ProjectsImagesProps> = ({ images }) => {
             transition: 'transform 0.3s ease-in-out',
           }}
         >
-          <CardBody className="group/card h-full w-full rounded-xl transition-transform duration-300 ease-in-out">
+          <CardBody className="group/card h-full w-full cursor-pointer rounded-2xl border border-white transition-transform duration-300 ease-in-out">
             <CardItem
-              //   translateZ={image.translateZ}
-              className="h-full w-full transform group-hover/card:h-[164px] group-hover/card:w-[340px]"
+              translateZ="50"
+              className="flex justify-between px-4 pb-2 pt-[14px]"
             >
-              <ProjectCard {...image} />
+              <h4 className="font-second-family text-m font-medium leading-s-16 tracking-s-2">
+                {image.name.toUpperCase()}
+              </h4>
+              <StarsBtnComponent
+                classNameDiv="gap-[6px]"
+                classNameIcons="h-[14px] w-[26px]"
+              />
+            </CardItem>
+            <div className="w-[332px] border-b border-white"></div>
+            <CardItem
+              translateZ="100"
+              //   translateZ={image.translateZ} group-hover/card:h-[164px] group-hover/card:w-[340px]
+              className="h-full w-full transform px-4 py-[19px]"
+            >
+              <Image
+                src={image.src}
+                className="object-cover"
+                alt={image.name}
+                // width={302}
+                // height={146}
+                style={{
+                  width: '300px',
+                  height: '146px',
+                }}
+              />
             </CardItem>
           </CardBody>
         </CardContainer>
