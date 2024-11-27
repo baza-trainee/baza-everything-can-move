d83082b51c2d757a4ef90e3f55f2d258dee4a9df
Viktoria Vonsovych <vikvons@gmail.com>
2024-11-27 22:22:13 +0100

    experience

--- src/components/sections/Experience/designedBySvitlana/Achievements/Achievements.tsx
+++ src/components/sections/Experience/designedBySvitlana/Achievements/Achievements.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import SectionTitle from '@/components/ui/SectionTitle';
 import Image from 'next/image';
-import NumberFlow from "@number-flow/react";
-import { achievementsBySvitlana } from '@/constants/achievements';
+import NumberFlow, { type Value } from '@number-flow/react'
+import { achievementsBySvitlana } from '@/constants/achievements'
+
 
 const Achievements = () => {
+  const [pageUploaded, setPageUploaded] = useState(false);
+  useEffect(() => {
+    setPageUploaded(true)
+  },[])
+
   return (
     <div className='py-20 lg:pt-[200px] pb-[96px] 2xl:pb-[100px]'> 
 
-      <div className='flex flex-col items-center 2xl:flex-row 2xl:justify-center 2xl:items-center gap-8 lg:gap-10 2xl:gap-6'>
+      <div className='flex flex-col items-center 2xl:flex-row 2xl:justify-center 2xl:items-center gap-8 lg:gap-10 2xl:gap-6 pb-[70px] 2xl:pb-[100px]'>
         <SectionTitle className='text-center' secondDesign={true}>Досягнення</SectionTitle>
         <Image
           src="/assets/images/Experience/star.png"
@@ -18,10 +24,16 @@ const Achievements = () => {
           className="h-[96px] w-[96px] lg:h-[87px] lg:w-[87px] 2xl:h-[96px] 2xl:w-[96px]"
         />
       </div>
-      <div>
+      <div className='flex flex-col items-center lg:flex-row lg:flex-wrap lg:justify-evenly'>
         {
           achievementsBySvitlana.map( (achievement, i) => {
-            return <NumberFlow key={`p_${i}`} value={achievement.numberValue} {...achievement}/>
+            return (
+              <div className='flex flex-col items-center justify-start pb-[64px] w-full lg:w-1/2 2xl:w-1/4'>
+                <NumberFlow key={`p_${i}`} value={pageUploaded ? achievement.numberValue : 0} trend={0} suffix='+' className='text-[76px] leading-[91.2px] font-semibold font-second-family p-0 text-center'/>
+                <p className='inline-block text-lg leading-[38.4px] font-medium font-second-family text-s-purple text-center pb-4'>{achievement.textValue}</p>
+                <p className='inline-block text-m leading-s-24 font-regular font-third-family text-center w-[264px]'>{achievement.text}</p>
+              </div>
+            )
           })
         }
       </div>
