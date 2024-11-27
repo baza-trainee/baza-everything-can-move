935aed67c82ac596063d2df671ed86e2f565a0c3
GannaKov <zlatta2000@gmail.com>
2024-11-27 18:11:01 +0100

    save

--- src/components/sections/Projects/DesignBySvitlana/ProjectsSection/SliderTablet2.tsx
+++ src/components/sections/Projects/DesignBySvitlana/ProjectsSection/SliderTablet2.tsx
@@ -1,14 +1,16 @@
 'use client';
 //1-5
 import React, { useState, useEffect, useRef } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-
+import { motion, AnimatePresence, useAnimationControls } from 'framer-motion';
+import { PanInfo } from 'framer-motion';
 import { ProjectsImagesProps } from './types';
 import ProjectCard from './ProjectCard';
 
 const SliderTablet2: React.FC<ProjectsImagesProps> = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [containerWidth, setContainerWidth] = useState(0);
+  const [direction, setDirection] = useState(0);
+
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   console.log(containerRef?.current?.getBoundingClientRect().width);
@@ -16,12 +18,6 @@ const SliderTablet2: React.FC<ProjectsImagesProps> = ({ images }) => {
   const gap = 31;
   const cardWidth = 332;
   const totalWidth = cardWidth + gap;
-
-  console.log(
-    containerWidth,
-    '-',
-    containerWidth / 2 - currentIndex * totalWidth - totalWidth / 2
-  );
 
   useEffect(() => {
     if (containerRef.current) {
@@ -47,19 +43,55 @@ const SliderTablet2: React.FC<ProjectsImagesProps> = ({ images }) => {
     setCurrentIndex(index);
   };
 
+  const handleDragEnd = (
+    event: MouseEvent | TouchEvent | PointerEvent,
+    info: PanInfo
+  ) => {
+    const swipeDistance = info.offset.x;
+    const swipeThreshold = totalWidth / 4;
+
+    if (swipeDistance > swipeThreshold) {
+      setDirection(-1); // to right
+      //   setCurrentIndex((prevIndex) =>
+      //     Math.min(prevIndex + 1, images.length - 1)
+      //   );
+      setCurrentIndex((prevIndex) => prevIndex - 1);
+    } else if (swipeDistance < -swipeThreshold) {
+      setDirection(1); // to left
+      //setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+  const imageVariants = {
+    initial: (direction: number) => ({
+      x: direction > 0 ? 100 : -100,
+    }),
+
+    animate: {
+      x: `${containerWidth / 2 - totalWidth / 2 - currentIndex * totalWidth}px`,
+    },
+
+    exit: (direction: number) => ({
+      x: direction > 1 ? -100 : 100,
+    }),
+  };
   return (
     <>
       {/* w-full */}
       <div className="relative mb-8 flex h-[225px] w-full justify-center overflow-hidden">
-        <AnimatePresence>
+        <AnimatePresence custom={direction}>
           <motion.div
             ref={containerRef}
-            //   drag="x"
-            //   dragConstraints={{ left: 0, right: 0 }}
-            //   onDragEnd={handleDragEnd}
-            animate={{
-              x: `${containerWidth / 2 - totalWidth / 2 - currentIndex * totalWidth}px`,
-            }}
+            variants={imageVariants}
+            custom={direction}
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            onDragEnd={handleDragEnd}
+            initial="initial"
+            // animate={{
+            //   x: `${containerWidth / 2 - totalWidth / 2 - currentIndex * totalWidth}px`,
+            // }}
+            animate="animate"
             transition={{ duration: 0.5 }}
             className="flex"
             // style={{
