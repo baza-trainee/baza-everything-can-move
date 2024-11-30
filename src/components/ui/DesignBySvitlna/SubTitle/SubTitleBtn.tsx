57f458922df88a632d565dadd617ca330dfcf09b
GannaKov <zlatta2000@gmail.com>
2024-11-30 18:20:33 +0100

    more or less

--- src/components/ui/DesignBySvitlna/SubTitle/SubTitleBtn.tsx
+++ src/components/ui/DesignBySvitlna/SubTitle/SubTitleBtn.tsx
@@ -17,40 +17,49 @@ const SubTitleBtn = ({ className, subTitleArr, color }: SubTitleBtnProps) => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % totalLength);
-    }, 1000);
+    }, 2000);
     return () => clearInterval(interval);
   }, []);
 
-  const itemArrVariants = {
-    initial: { opacity: 0 },
-    animate: { opacity: 1 },
-    exit: { opacity: 0 },
-  };
+  // const itemArrVariants = {
+  //   initial: { opacity: 0 },
+  //   animate: { opacity: 1 },
 
+  //   exit: { opacity: 0 },
+  // };
   return (
     <div
       className={clsx('relative inline-flex h-8 min-w-32 lg:h-11', className)}
     >
-      <AnimatePresence initial={false}>
+      <AnimatePresence mode="wait">
         {subTitleArr && (
-          <motion.button
-            key={subTitleArr[currentIndex]}
-            variants={itemArrVariants}
-            initial="enter"
-            animate="center"
-            transition={{
-              duration: 0.5,
-              ease: 'easeOut',
-            }}
-            className={clsx(
-              'absolute inline-flex h-8 cursor-default items-center justify-center rounded-[100px] border px-4 font-second-family text-m font-bold uppercase leading-s-100 tracking-s-2 lg:h-11 lg:px-8 lg:text-md lg:font-medium',
-              color === 'black' && 'border-black text-black',
-              color === 'white' && 'border-white text-white'
-            )}
-            style={{ whiteSpace: 'nowrap' }}
-          >
-            {subTitleArr[currentIndex]}
-          </motion.button>
+          <div className="absolute left-0 top-0 inline-flex h-8 items-center justify-center rounded-[100px] border px-4 lg:h-11 lg:px-8">
+            <motion.div
+              key={subTitleArr[currentIndex]}
+              // variants={itemArrVariants}
+              // initial="enter"
+              // animate="animate"
+              // exit="exit"
+              animate={{
+                opacity: [0.2, 1],
+              }}
+              transition={{
+                duration: 2,
+                ease: 'easeInOut',
+              }}
+              // transition={{ duration: 2, times: [0, 0.5, 1] }}
+              className={clsx(
+                'font-second-family text-m font-bold uppercase leading-s-100 tracking-s-2 lg:text-md lg:font-medium',
+                color === 'black' && 'border-black text-black',
+                color === 'white' && 'border-white text-white'
+              )}
+              style={{
+                whiteSpace: 'nowrap',
+              }}
+            >
+              {subTitleArr[currentIndex]}
+            </motion.div>
+          </div>
         )}
       </AnimatePresence>
     </div>
