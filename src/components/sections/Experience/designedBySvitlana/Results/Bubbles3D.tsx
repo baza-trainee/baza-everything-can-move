d6f2e82fa4c915e7f880e54f6bf7a07183b31ead
Viktoria Vonsovych <vikvons@gmail.com>
2024-12-02 23:17:51 +0100

    experience

--- src/components/sections/Experience/designedBySvitlana/Results/Bubbles3D.tsx
+++ src/components/sections/Experience/designedBySvitlana/Results/Bubbles3D.tsx
@@ -1,23 +1,24 @@
 'use client'
-import React, {useEffect, useRef} from 'react'
-import * as THREE from 'three';
+// import React, {useEffect, useRef} from 'react'
+// import * as THREE from 'three';
 
 const Bubbles3D = () => {
-    const canvas = useRef(null);
+    // const canvas = useRef(null);
     // const screenWidth = window.innerWidth;
 
-    useEffect(()=>{
-        const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 2000 );
-		camera.position.z = 1000;
-		const scene = new THREE.Scene();
-		scene.fog = new THREE.FogExp2( 0x000000, 0.0008 );
-		const geometry = new THREE.BufferGeometry();
-    }, [])
+    // useEffect(()=>{
+    //     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 2000 );
+	// 	camera.position.z = 1000;
+	// 	const scene = new THREE.Scene();
+	// 	scene.fog = new THREE.FogExp2( 0x000000, 0.0008 );
+	// 	const geometry = new THREE.BufferGeometry();
+    // }, [])
 
   return (
-    <div ref={canvas}>
+    <div>Bubbles3D</div>
+    // <div ref={canvas}>
     
-    </div>
+    // </div>
   )
 }
 
--- src/components/sections/Experience/designedBySvitlana/Results/Results.tsx
+++ src/components/sections/Experience/designedBySvitlana/Results/Results.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import SectionTitle from '@/components/ui/SectionTitle';
 import Bubbles3D from './Bubbles3D';
 
