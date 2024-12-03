af42ae8c00a6ccba43270610e9a2de74bccbbef4
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-12-03 23:53:14 +0100

    Update TeamSectionSV.tsx

--- src/components/sections/PageTeam/desingeBySvitlana/TeamSection/TeamSectionSV.tsx
+++ src/components/sections/PageTeam/desingeBySvitlana/TeamSection/TeamSectionSV.tsx
@@ -1,20 +1,23 @@
 import Container from '@/components/ui/DesignBySvitlna/Container';
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import GloboTeam from './components/GloboTeam';
-// import ListTeam from './components/ListTeam';
+import ListTeam from './components/ListTeam';
 import SwiperSlider from './components/SwiperSlider';
 
 function TeamSectionSV() {
+  const [isSwiper, togleSwiper] = useState(true);
   return (
     <section className="relative overflow-hidden border-t border-t-white pb-[140px] pt-20 lg:pb-[178px] lg:pt-[160px] 2xl:pb-[110px] 2xl:pt-[100px]">
       <Container className="">
         <Header />
         <GloboTeam />
       </Container>
-      {/* <ListTeam /> */}
-      <SwiperSlider />
+      {isSwiper ? <ListTeam /> : <SwiperSlider />}
       {/* <ListTeamcopy /> */}
+      <button type="button" onClick={() => togleSwiper(!isSwiper)}>
+        зміни слайдер
+      </button>
     </section>
   );
 }
