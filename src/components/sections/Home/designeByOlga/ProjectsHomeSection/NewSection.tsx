fc0d0ea9925d34965fe42deb3cd188ddb47380b1
GannaKov <zlatta2000@gmail.com>
2024-12-04 23:21:23 +0100

    Olga preparation

--- src/components/sections/Home/designeByOlga/ProjectsHomeSection/NewSection.tsx
+++ src/components/sections/Home/designeByOlga/ProjectsHomeSection/NewSection.tsx
@@ -1,15 +1,87 @@
+'use client';
+import React, { useRef } from 'react';
+import { useTransform, useScroll, motion } from 'framer-motion';
+import { IMAGES_HOME_PROJECTS } from '@/constants/images/imagesSrc';
+import Image from 'next/image';
+import Link from 'next/link';
 import SectionTitle from '@/components/ui/SectionTitle';
-import React from 'react';
+import {
+  CardBody,
+  CardContainer,
+  CardItem,
+} from '@/components/ui/new-3d-third';
 
-import { PCardsThird } from './PCardsThird';
+// import { PCardsThird } from './PCardsThird';
 
 const NewSection: React.FC = () => {
-  return (
-    <section className="pb-[100px] 2xl:pb-[120px]">
-      <div className="container">
-        <SectionTitle className="mb-8">проєкти бази</SectionTitle>
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
 
-        <PCardsThird />
+    offset: ['start end', 'start start'],
+  });
+  const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+  return (
+    <section ref={containerRef} className="pb-[100px] 2xl:pb-[120px]">
+      <div className="container">
+        <SectionTitle className="mb-8">New Version</SectionTitle>
+        <div className="hidden w-full justify-center lg:flex">
+          <div className="lg:relative lg:block lg:h-[338px] lg:w-[620px]">
+            {IMAGES_HOME_PROJECTS.map((image, ind) => (
+              // cardContainer or imageContainer or card
+              <div
+                key={image.name + ind}
+                style={{
+                  top: `calc( ${ind * 25}px)`,
+                }}
+                className="relative h-[190px] w-[340px] rounded-sm border border-white bg-olga-green text-olga-grey"
+              >
+                <motion.div className="">
+                  <div className="">
+                    <p>Card {ind}</p>
+                  </div>
+                </motion.div>
+              </div>
+              //   <CardContainer
+              //     key={image.name + ind}
+              //     className="absolute w-fit"
+              //     style={{
+              //       top: image.topTablet,
+              //       left: image.leftTablet,
+              //       width: `${image.widthTablet}px`,
+              //       height: `${image.heightTablet}px`,
+              //       transition: 'transform 0.3s ease-in-out',
+              //     }}
+              //   >
+              //     <CardBody className="group/card h-full w-full rounded-xl transition-transform duration-300 ease-in-out">
+              //       <CardItem
+              //         translateZ={image.translateZ}
+              //         className="h-full w-full transform group-hover/card:h-[164px] group-hover/card:w-[340px]"
+              //       >
+              //         <Link
+              //           href={image.link}
+              //           target="_blank"
+              //           rel="noopener noreferrer"
+              //           className="cursor-none"
+              //         >
+              //           <Image
+              //             src={image.src}
+              //             className="rounded-xl object-cover group-hover/card:shadow-xl"
+              //             alt={image.name}
+              //             style={{
+              //               width: '100%',
+              //               height: '100%',
+              //               transition:
+              //                 'width 0.3s ease-in-out, height 0.3s ease-in-out',
+              //             }}
+              //           />
+              //         </Link>
+              //       </CardItem>
+              //     </CardBody>
+              //   </CardContainer>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
