365afb78d7baaee8aa8f89eb0483b9701f062c98
GannaKov <zlatta2000@gmail.com>
2024-12-05 01:11:54 +0100

    save scroll fake  cards

--- src/components/sections/Home/designeByOlga/ProjectsHomeSection/NewSection.tsx
+++ src/components/sections/Home/designeByOlga/ProjectsHomeSection/NewSection.tsx
@@ -1,6 +1,7 @@
 'use client';
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useTransform, useScroll, motion } from 'framer-motion';
+import { cubicBezier } from 'motion';
 import { IMAGES_HOME_PROJECTS } from '@/constants/images/imagesSrc';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -18,30 +19,37 @@ const NewSection: React.FC = () => {
   const { scrollYProgress } = useScroll({
     target: containerRef,
 
-    offset: ['start end', 'start start'],
+    offset: ['start end', 'end start'],
   });
-  const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+
+  const imageScale = useTransform(scrollYProgress, [0, 0.5, 1], [0.5, 1, 0.5]);
   return (
-    <section ref={containerRef} className="pb-[100px] 2xl:pb-[120px]">
+    <section
+      ref={containerRef}
+      className="border border-t border-s-purple pb-[100px] 2xl:pb-[120px]"
+    >
       <div className="container">
         <SectionTitle className="mb-8">New Version</SectionTitle>
         <div className="hidden w-full justify-center lg:flex">
           <div className="lg:relative lg:block lg:h-[338px] lg:w-[620px]">
             {IMAGES_HOME_PROJECTS.map((image, ind) => (
               // cardContainer or imageContainer or card
-              <div
+              <motion.div
                 key={image.name + ind}
                 style={{
                   top: `calc( ${ind * 25}px)`,
+                  scale: imageScale,
                 }}
-                className="relative h-[190px] w-[340px] rounded-sm border border-white bg-olga-green text-olga-grey"
+                transition={{
+                  ease: 'linear',
+                  duration: 0.5,
+                }}
+                className="absolute rounded-xl border border-white bg-olga-green text-olga-grey"
               >
-                <motion.div className="">
-                  <div className="">
-                    <p>Card {ind}</p>
-                  </div>
-                </motion.div>
-              </div>
+                <div className="h-[190px] w-[340px]">
+                  <p>Card {ind}</p>
+                </div>
+              </motion.div>
               //   <CardContainer
               //     key={image.name + ind}
               //     className="absolute w-fit"
