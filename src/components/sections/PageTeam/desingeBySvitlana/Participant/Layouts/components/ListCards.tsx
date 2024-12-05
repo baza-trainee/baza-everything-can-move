f866a0bd7161bca922a69c67335b33b8ca076fdd
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-12-05 22:43:58 +0100

    updated scroll y width

--- src/components/sections/PageTeam/desingeBySvitlana/Participant/Layouts/components/ListCards.tsx
+++ src/components/sections/PageTeam/desingeBySvitlana/Participant/Layouts/components/ListCards.tsx
@@ -1,32 +1,35 @@
 import { motion } from 'framer-motion';
 import { data } from './data';
 import { Card } from './Card';
+import { forwardRef, HTMLAttributes } from 'react';
 
-type Props = { paddingTop: number };
+type Props = { paddingTop: number } & HTMLAttributes<HTMLUListElement>;
 
-function ListCards({ paddingTop }: Props) {
-  return (
-    <motion.ul className="flex w-full justify-between">
-      {data.map((item, index) => {
-        return (
-          <motion.li
-            key={index}
-            style={{
-              paddingTop: `${paddingTop * index}px`,
-            }}
-          >
-            <Card
-              value={item.value}
-              text={item.text}
-              bg={item.bg}
-              border={item.border}
-              textColor={item.textColor}
-            />
-          </motion.li>
-        );
-      })}
-    </motion.ul>
-  );
-}
+const ListCards = forwardRef<HTMLUListElement, Props>(
+  ({ paddingTop, ...props }, ref) => {
+    return (
+      <ul {...props} ref={ref} className="flex w-full justify-between">
+        {data.map((item, index) => {
+          return (
+            <motion.li
+              key={index}
+              style={{
+                paddingTop: `${paddingTop * index}px`,
+              }}
+            >
+              <Card
+                value={item.value}
+                text={item.text}
+                bg={item.bg}
+                border={item.border}
+                textColor={item.textColor}
+              />
+            </motion.li>
+          );
+        })}
+      </ul>
+    );
+  }
+);
 
 export default ListCards;
