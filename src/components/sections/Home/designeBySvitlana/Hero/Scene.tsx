348bea5073e0e24f9e76556aa898132e1186470c
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-12-08 11:37:45 +0200

    changed the model and its adaptor

--- src/components/sections/Home/designeBySvitlana/Hero/Scene.tsx
+++ src/components/sections/Home/designeBySvitlana/Hero/Scene.tsx
@@ -1,43 +1,60 @@
 'use client';
 import { Canvas } from '@react-three/fiber';
 import React, { Suspense, useEffect, useState } from 'react';
-import Model from './Model';
+import { Center } from '@react-three/drei';
+import Model from './ModelCopy';
 
 export default function Scene() {
   const [scrollY, setScrollY] = useState(0);
-  const [isLargeScreen, setIsLargeScreen] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    // Перевірка ширини екрану
-    const checkScreenSize = () => {
-      setIsLargeScreen(window.innerWidth > 769);
+    // Оновлення ширини екрану при зміні розміру
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
     };
 
-    // Ініціалізація перевірки та підписка на зміни розміру екрану
-    checkScreenSize();
-    window.addEventListener('resize', checkScreenSize);
+    window.addEventListener('resize', handleResize);
 
     return () => {
-      window.removeEventListener('resize', checkScreenSize);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (isLargeScreen) {
-        setScrollY(window.scrollY); // Встановлюємо поточну позицію скролу
-      }
+      setScrollY(window.scrollY); // Встановлюємо поточну позицію скролу
     };
 
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [isLargeScreen]);
+  }, []);
 
-  // Розрахунок зміщення залежно від позиції скролу
-  const translateX = isLargeScreen ? Math.max(-70, 20 - scrollY / 2) : 0;
-  const translateY = isLargeScreen ? Math.min(130, scrollY / 2) : 0;
+  // Розрахунок зміщення залежно від ширини екрану та позиції скролу
+  const calculateTranslateX = () => {
+    if (screenWidth > 1200) {
+      return Math.max(-40, 20 - scrollY / 2); // Для великих екранів
+    } else if (screenWidth > 768) {
+      return Math.max(-40, 20 - scrollY / 2); // Для середніх екранів
+    } else {
+      // return Math.max(-10, 5 - scrollY / 5); // Для маленьких екранів
+    }
+  };
+
+  const calculateTranslateY = () => {
+    if (screenWidth > 1200) {
+      return Math.min(130, scrollY / 2); // Для великих екранів
+    } else if (screenWidth > 768) {
+      return Math.min(120, scrollY / 3); // Для середніх екранів
+    } else {
+      // return Math.min(50, scrollY / 4); // Для маленьких екранів
+    }
+  };
+
+  const translateX = calculateTranslateX();
+  const translateY = calculateTranslateY();
 
   return (
     <>
@@ -46,12 +63,14 @@ export default function Scene() {
           transform: `translate(${translateX}%, ${translateY}%)`,
           transition: 'transform 1.9s ease-out',
         }}
-        className="h-[700px] 2xl:w-[50%] lg:w-[80%] w-[100%] z-10 left-[30%] absolute"
+        className="absolute left-0 -top-24 z-10 h-[700px] w-[100%] lg:w-[70%] xl:-top-64 xl:left-[30%] 2xl:w-[50%]"
       >
-        <Canvas className="w-[100%] h-[60%]">
-          <directionalLight position={[1, 16, 12]} intensity={0} />
+        <Canvas gl={{ antialias: true }} dpr={[1, 1.5]}>
+          <directionalLight position={[-5, -5, 5]} intensity={5} />
           <Suspense fallback={null}>
-            <Model />
+            <Center position={[-0.5, -0.5, 0]}>
+              <Model />
+            </Center>
           </Suspense>
         </Canvas>
       </div>
