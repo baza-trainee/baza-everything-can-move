9bdde58629efa82d3b5eb77ffb56d0daf56b4bcd
bohdanalu <b_dana@ukr.net>
2024-12-13 19:41:47 +0200

    readd icon star

--- src/constants/icons/icons.ts
+++ src/constants/icons/icons.ts
@@ -7,6 +7,7 @@ import white_violet_star_lg from '../../../public/assets/icons/white_violet_star
 import white_violet_star_2xl from '../../../public/assets/icons/white_violet_star_2xl.svg';
 import violet_white_star_sm from '../../../public/assets/icons/violet_white_star_sm.svg';
 import violet_white_star_lg from '../../../public/assets/icons/violet_white_star_lg.svg';
+import white_star from "../../../public/assets/icons/white_star.svg"
 
 
 
@@ -20,4 +21,5 @@ export const ICONS = {
   WHITE_VIOLET_STAR_2XL: white_violet_star_2xl,
   VIOLET_WHITE_STAR_SM: violet_white_star_sm,
   VIOLET_WHITE_STAR_LG: violet_white_star_lg,
+  WHITE_STAR: white_star
 };
