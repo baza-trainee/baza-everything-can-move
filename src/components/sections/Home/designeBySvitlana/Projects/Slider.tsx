bf3def9b37d4fceebefa005beca613743ad8993d
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-12-16 21:03:12 +0200

    add import

--- src/components/sections/Home/designeBySvitlana/Projects/Slider.tsx
+++ src/components/sections/Home/designeBySvitlana/Projects/Slider.tsx
@@ -204,6 +204,7 @@ import React, { useRef } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+
 import { ButtonSlide } from '../../../../ui/SwiperFoto/components/ButtonSlider';
 import './style.css';
 
