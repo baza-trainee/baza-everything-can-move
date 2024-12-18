12144cc1438205298db3f5fcbd272f52e8a9fcf1
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-12-18 21:57:25 +0100

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { useDesignStore } from '@/useDesignStore';
 
 import PartnersSection from '@/components/sections/Projects/DesignByOlga/PartnersSection/PartnersSection';
-import ProjectsSection from '@/components/sections/Projects/ProjectsSection/ProjectsSection';
+import ProjectsSection from '@/components/sections/Projects/DesignByOlga/ProjectsSection/ProjectsSection';
 import BirdsComponent from '@/components/sections/Projects/DesignBySvitlana/Birds/BirdsComponent';
 import ProjectsSectionSecond from '@/components/sections/Projects/DesignBySvitlana/ProjectsSection/ProjectsSection';
 import PurposeSection from '@/components/sections/Projects/DesignBySvitlana/PurposeSection/PurposeSection';
