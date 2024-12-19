f3171c12e053d6eec20790f2a40011c80ff8a228
GannaKov <zlatta2000@gmail.com>
2024-12-19 17:23:56 +0100

    save position in newOlgaSlider

--- src/components/sections/Home/designeByOlga/ProjectsHomeSection/MobileCarousel/MobileCarousel2.tsx
+++ src/components/sections/Home/designeByOlga/ProjectsHomeSection/MobileCarousel/MobileCarousel2.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useCallback, useRef, } from 'react'; //useEffect,
+import React, { useState, useCallback, useRef } from 'react'; //useEffect,
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -8,6 +8,7 @@ import { PanInfo } from 'framer-motion';
 //import { ICONS_SHARED } from '@/constants/icons/iconsSrc';
 import { ImagesHomeProjectsProps } from '@/constants/images/imagesSrc';
 import { ButtonSlide } from '@/components/ui/SwiperFoto';
+import clsx from 'clsx';
 
 interface ProjectsSliderProps {
   images: ImagesHomeProjectsProps;
@@ -20,8 +21,8 @@ const MobileCarousel2: React.FC<ProjectsSliderProps> = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const [direction, setDirection] = useState(0);
-//   const cardWidth = 320;
-//   const totalWidth = 343;
+  //   const cardWidth = 320;
+  //   const totalWidth = 343;
   const totalImages = images.length;
 
   const imageVariants = {
@@ -101,46 +102,66 @@ const MobileCarousel2: React.FC<ProjectsSliderProps> = ({ images }) => {
       );
     }
   };
+  const positions = ['left', 'center', 'right']; //cI=1 left=0 center=1 right=2???
+  // const positions = ['right', 'center', 'left']; //cI=1 left=2 center=1 right=0???
+  const getPositionIndex = (baseIndex: number, offset: number) => {
+    return (baseIndex + offset + totalImages) % totalImages;
+  };
   return (
     <div
       className="w-full flex-col items-center justify-center lg:hidden"
       ref={containerRef}
     >
-      {/* w-[343px] */}
+      {/* <div className="h-[193px] "> */}
+      {/* */}
       <div className="relative mb-6 flex h-[193px] w-[343px] items-center justify-center overflow-hidden">
         <AnimatePresence custom={direction}>
-          <motion.div
-            key={`title-${currentIndex}`}
-            variants={imageVariants}
-            custom={direction}
-            initial="enter"
-            animate="center"
-            exit="exit"
-            drag="x"
-            dragConstraints={containerRef}
-            //dragElastic={0}
-            onDragEnd={handleDragEnd}
-            // h-[164px] w-[320px]
-            className="absolute duration-500 ease-linear"
-          >
-            <Link
-              href={images[currentIndex].link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="h-[164px] w-[320px]"
-            >
-              <Image
-                src={images[currentIndex].src}
-                alt={`Image ${images[currentIndex].name}`}
-                className="object-cover"
-                style={{ width: '100%' }}
-              />
-            </Link>
-          </motion.div>
+          {positions.map((position, posIndex) => {
+            const imageIndex = getPositionIndex(currentIndex, posIndex - 1);
+
+            return (
+              <motion.div
+                key={`image-${imageIndex}`}
+                variants={imageVariants}
+                custom={direction}
+                initial={posIndex === 1 && 'center'}
+                //animate="center"
+                //exit="exit"
+                drag="x"
+                dragConstraints={containerRef}
+                //dragElastic={0}
+                onDragEnd={handleDragEnd}
+                // h-[164px] w-[320px]
+                className={clsx(
+                  // top-0
+                  'absolute w-[320px] duration-500 ease-linear',
+                  posIndex === 0 && 'left-0 top-0 w-[160px] z-0',
+                  posIndex === 1 && 'top-0 w-[320px] z-10',
+
+                  posIndex === 2 && 'right-0 top-0 w-[160px] z-0'
+                )}
+              >
+                <Link
+                  href={images[imageIndex].link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="h-[164px] w-[320px]"
+                >
+                  <Image
+                    src={images[imageIndex].src}
+                    alt={`Image ${imageIndex}`}
+                    className="object-cover"
+                    style={{ width: '100%' }}
+                  />
+                </Link>
+              </motion.div>
+            );
+          })}
 
           {/* image */}
         </AnimatePresence>
       </div>
+      {/* </div> */}
       {/* buttons div */}
       <div className="flex justify-center gap-5">
         <ButtonSlide onClick={handleBack} ariaLabel="кнопка попереднє фото" />
