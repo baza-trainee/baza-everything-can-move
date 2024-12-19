7015614c8b46ae98f488482772e78b6bb834f6e4
bohdanalu <b_dana@ukr.net>
2024-12-19 13:48:22 +0200

    change font size

--- src/components/ui/DesignBySvitlna/AnimatedTitle.tsx
+++ src/components/ui/DesignBySvitlna/AnimatedTitle.tsx
@@ -144,7 +144,7 @@ function AnimatedTitle({
     <div ref={ref}>
       <div
         className={cn(
-          'traking-s-2 font-second-family text-xlg font-semibold uppercase leading-o-150 2xl:text-3xl lg:leading-o-120',
+          'traking-s-2 font-second-family text-xlg font-semibold uppercase leading-o-150 lg:text-3xl lg:leading-o-120',
           className
         )}
       >
