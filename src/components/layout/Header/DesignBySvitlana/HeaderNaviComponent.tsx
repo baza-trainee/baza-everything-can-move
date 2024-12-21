c46a245b96febdaa776ed6e7f818a4f682f7200f
GannaKov <zlatta2000@gmail.com>
2024-12-21 19:26:14 +0100

    save set timeout

--- src/components/layout/Header/DesignBySvitlana/HeaderNaviComponent.tsx
+++ src/components/layout/Header/DesignBySvitlana/HeaderNaviComponent.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 
 import { ICONS_SRC } from '@/constants/icons/iconsSrc';
@@ -11,20 +12,32 @@ const HeaderNaviComponentSvitlana: React.FC<HeaderNavigationProps> = ({
   handleMenuClick,
   openMenu,
 }) => {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    if (openMenu) {
+      setIsAnimating(true);
+      const timeout = setTimeout(() => {
+        setIsAnimating(false);
+      }, 500);
+      return () => clearTimeout(timeout);
+    } else {
+      setIsAnimating(false);
+    }
+  }, [openMenu]);
+
+  console.log('is A', isAnimating);
   return (
     // <div className="max-h-screen overflow-y-auto">
     <div
       className={clsx(
         //   h-dvh??  h-[812px] lg:h-[1024px] 2xl:h-[860px]
-        'fixed top-0 z-[10000] flex h-[812px] w-screen flex-col overflow-y-auto bg-s-gray font-second-family text-white transition-transform duration-500 ease-linear lg:h-[1024px] 2xl:right-0 2xl:top-0 2xl:h-[860px] 2xl:w-[730px]',
-        openMenu
-          ? 'translate-y-0'
-          : //-translate-y-full
-            '-translate-y-[815px] lg:-translate-y-[1030px] 2xl:-translate-y-[864px]'
-        //'pb-[85px] lg:pb-[212px] 2xl:pb-[130px]' //max-h-screen
+        'fixed top-0 z-[10000] flex w-screen flex-col bg-s-gray font-second-family text-white transition-transform duration-500 ease-linear 2xl:right-0 2xl:top-0 2xl:w-[730px]',
+        openMenu ? 'translate-y-0' : '-translate-y-full',
+        isAnimating ? 'overflow-visible' : 'max-h-screen overflow-y-auto'
       )}
     >
-      <div className="max-h-screen overflow-y-auto pb-[85px] lg:pb-[212px] 2xl:pb-[130px]">
+      <div className="flex-1 overflow-y-auto pb-[85px] lg:pb-[212px] 2xl:pb-[130px]">
         {' '}
         <div className="w-full px-6 pb-[49px] pt-6 lg:pb-[134px] 2xl:px-20 2xl:pb-[38px] 2xl:pt-8">
           <button
