9984483e2de1a804fd32ddd92effa6e5b7ec45ca
GannaKov <zlatta2000@gmail.com>
2024-12-26 19:54:48 +0100

    save text and gray circle style

--- src/components/ui/DesignBySvitlna/BtnAnimated.tsx
+++ src/components/ui/DesignBySvitlna/BtnAnimated.tsx
@@ -14,18 +14,40 @@ const BtnAnimated = () => {
       {/* gray cicle */}
       <motion.div
         //   top-[-214px]
-        className="absolute left-0 right-0 z-0 mx-auto h-[214px] w-[214px] rounded-full bg-s-gray"
+        className="absolute left-0 right-0 z-0 mx-auto rounded-full bg-s-gray transition-all duration-300 ease-out"
+        // h-[214px] w-[214px]  variants={{
+        //   rest: { y: '-120%', transition: { duration: 0.5 } },
+        //   hover: { y: '-45%', transition: { duration: 0.5 } },
+        // }}
         variants={{
-          rest: { y: '-120%', transition: { duration: 0.5 } },
-          hover: { y: '-45%', transition: { duration: 0.5 } },
+          rest: {
+            y: '-38px',
+            width: 28,
+            height: 28,
+            //transition: { duration: 0.3 },
+          },
+          hover: {
+            y: '-83px',
+            width: 214,
+            height: 214,
+            //transition: { duration: 0.3 },
+          },
         }}
       />
       {/*gray text */}
       <motion.span
         className="absolute inset-0 z-10 flex items-center justify-center font-third-family text-md uppercase leading-s-100 tracking-s-1 text-s-gray"
         variants={{
-          rest: { y: '0px', opacity: 1, transition: { duration: 0.3 } },
-          hover: { y: '-20px', opacity: 0, transition: { duration: 0.3 } },
+          rest: {
+            y: '0px',
+            opacity: 1,
+            transition: { duration: 0.4, ease: 'linear' },
+          },
+          hover: {
+            y: '-48px',
+            opacity: 0,
+            transition: { duration: 0.4, ease: 'linear' },
+          },
         }}
       >
         переглянути
@@ -34,8 +56,16 @@ const BtnAnimated = () => {
       <motion.span
         className="absolute inset-0 z-10 flex items-center justify-center font-third-family text-md uppercase leading-s-100 tracking-s-1 text-white"
         variants={{
-          rest: { y: '20px', opacity: 0, transition: { duration: 0.3 } },
-          hover: { y: '0px', opacity: 1, transition: { duration: 0.3 } },
+          rest: {
+            y: '48px',
+            opacity: 0,
+            transition: { duration: 0.4, ease: 'linear' },
+          },
+          hover: {
+            y: '0px',
+            opacity: 1,
+            transition: { duration: 0.4, ease: 'linear' },
+          },
         }}
       >
         переглянути
