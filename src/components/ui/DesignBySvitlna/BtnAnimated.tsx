224c19fb6deaeb0ba78f6290b322ec7e8e76f542
GannaKov <zlatta2000@gmail.com>
2024-12-26 21:25:19 +0100

    save arrow style

--- src/components/ui/DesignBySvitlna/BtnAnimated.tsx
+++ src/components/ui/DesignBySvitlna/BtnAnimated.tsx
@@ -5,75 +5,84 @@ import { ICONS_SHARED } from '@/constants/icons/iconsSrc';
 const BtnAnimated = () => {
   return (
     // btn
-    <motion.div
-      className="relative inline-block h-12 w-[214px] cursor-pointer items-center justify-center overflow-hidden rounded-full border border-white bg-white text-center"
-      initial="rest"
-      whileHover="hover"
-      whileTap="hover"
-      animate="rest"
-    >
-      {/* gray cicle */}
+    // <div className="h-12 w-[228px]">
+    //  {/* overflow-hidden */}
+    <div className="flex text-white hover:text-s-gray">
+      {' '}
       <motion.div
-        //   top-[-214px]
-        className="absolute left-0 right-0 z-0 mx-auto rounded-full bg-s-gray text-white transition-all duration-300 ease-out hover:text-s-gray"
-        // h-[214px] w-[214px]  variants={{
-        //   rest: { y: '-120%', transition: { duration: 0.5 } },
-        //   hover: { y: '-45%', transition: { duration: 0.5 } },
-        // }}
-        variants={{
-          rest: {
-            y: '-38px',
-            width: 28,
-            height: 28,
-            //transition: { duration: 0.3 },
-          },
-          hover: {
-            y: '-83px',
-            width: 214,
-            height: 214,
-            //transition: { duration: 0.3 },
-          },
-        }}
-      />
-      {/*gray text */}
-      <motion.span
-        className="absolute inset-0 z-10 flex items-center justify-center font-third-family text-md uppercase leading-s-100 tracking-s-1 text-s-gray"
-        variants={{
-          rest: {
-            y: '0px',
-            opacity: 1,
-            transition: { duration: 0.4, ease: 'linear' },
-          },
-          hover: {
-            y: '-48px',
-            opacity: 0,
-            transition: { duration: 0.4, ease: 'linear' },
-          },
-        }}
+        className="relative inline-block h-12 w-[214px] cursor-pointer overflow-hidden rounded-full border border-white bg-white text-center"
+        initial="rest"
+        whileHover="hover"
+        whileTap="hover"
+        animate="rest"
       >
-        переглянути
-      </motion.span>
-      {/* white text */}
-      <motion.span
-        className="absolute inset-0 z-10 flex items-center justify-center font-third-family text-md uppercase leading-s-100 tracking-s-1 text-white"
-        variants={{
-          rest: {
-            y: '48px',
-            opacity: 0,
-            transition: { duration: 0.4, ease: 'linear' },
-          },
-          hover: {
-            y: '0px',
-            opacity: 1,
-            transition: { duration: 0.4, ease: 'linear' },
-          },
-        }}
-      >
-        переглянути
-      </motion.span>
-      {/* -right-[14px] top-[10px] */}
-      <ICONS_SHARED.ARROW_BTN_ICON className="absolute z-30 top-[10px]" />
-    </motion.div>
+        {/* gray cicle */}
+        <motion.div
+          //
+          className="absolute left-0 right-0 z-0 mx-auto rounded-full bg-s-gray transition-all duration-300 ease-out"
+          // h-[214px] w-[214px]  variants={{
+          //   rest: { y: '-120%', transition: { duration: 0.5 } },
+          //   hover: { y: '-45%', transition: { duration: 0.5 } },
+          // }}
+          variants={{
+            rest: {
+              y: '-38px',
+              width: 28,
+              height: 28,
+              //transition: { duration: 0.3 },
+            },
+            hover: {
+              y: '-83px',
+              width: 214,
+              height: 214,
+              //transition: { duration: 0.3 },
+            },
+          }}
+        />
+        {/*gray text */}
+        <motion.span
+          className="absolute inset-0 z-10 flex items-center justify-center font-third-family text-md uppercase leading-s-100 tracking-s-1 text-s-gray"
+          variants={{
+            rest: {
+              y: '0px',
+              opacity: 1,
+              transition: { duration: 0.4, ease: 'linear' },
+            },
+            hover: {
+              y: '-48px',
+              opacity: 0,
+              transition: { duration: 0.4, ease: 'linear' },
+            },
+          }}
+        >
+          переглянути
+        </motion.span>
+        {/* white text */}
+        <motion.span
+          className="absolute inset-0 z-10 flex items-center justify-center font-third-family text-md uppercase leading-s-100 tracking-s-1 text-white"
+          variants={{
+            rest: {
+              y: '48px',
+              opacity: 0,
+              transition: { duration: 0.4, ease: 'linear' },
+            },
+            hover: {
+              y: '0px',
+              opacity: 1,
+              transition: { duration: 0.4, ease: 'linear' },
+            },
+          }}
+        >
+          переглянути
+        </motion.span>
+        {/*  stroke-current transition-colors delay-200*/}
+      </motion.div>
+      <div className="relative h-12">
+        <ICONS_SHARED.ARROW_BTN_ICON className="absolute -right-[14px] top-[10px] z-30" />
+      </div>
+    </div>
+
+    // {/* </div> */}
   );
 };
 
