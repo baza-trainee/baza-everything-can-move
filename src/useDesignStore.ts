d57f7b5a9d10daee8f4c38286fe19e6471039dff
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-12-29 10:51:22 +0100

    Update useDesignStore.ts

--- src/useDesignStore.ts
+++ src/useDesignStore.ts
@@ -1,13 +1,13 @@
 import { create } from 'zustand';
 
-export type DesignTypes = 'designByOlga' | 'designBySvitlana' | 'initial';
+export type designTypes = 'designByOlga' | 'designBySvitlana' | 'initial';
 
 export type ChangDesign = boolean;
 
 interface DesignStoreState {
   isChangingDesign: ChangDesign;
-  designType: DesignTypes;
-  setDesignType: (type: DesignTypes) => void;
+  designType: designTypes;
+  setDesignType: (type: designTypes) => void;
   toggleDesignType: () => void;
   toggleIsChangingDesign: (isChangingDesignProps: ChangDesign) => void;
 }
@@ -15,7 +15,7 @@ interface DesignStoreState {
 export const useDesignStore = create<DesignStoreState>((set) => ({
   designType: 'initial',
   isChangingDesign: false,
-  setDesignType: (type: DesignTypes) => set({ designType: type }),
+  setDesignType: (type: designTypes) => set({ designType: type }),
   toggleDesignType: () =>
     set((state) => ({
       designType:
