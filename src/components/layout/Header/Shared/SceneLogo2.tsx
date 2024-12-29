c9327cdc85beb15a9254e7741ee5dd30c558adcf
GannaKov <zlatta2000@gmail.com>
2024-12-30 00:04:12 +0100

    save

---
+++ src/components/layout/Header/Shared/SceneLogo2.tsx
@@ -0,0 +1,20 @@
+import { Canvas } from '@react-three/fiber';
+import ModelLogo2 from './ModelLogo';
+
+import { Suspense } from 'react';
+//import { Center } from '@react-three/drei';
+
+function SceneLogo2() {
+  return (
+    <Canvas gl={{ antialias: true }} dpr={[1, 1.5]}>
+      <directionalLight position={[-5, -5, 5]} intensity={5} />
+      <Suspense fallback={null}>
+        {/* <Center position={[-0.5, -0.5, 0]}> */}
+        <ModelLogo2 />
+        {/* </Center> */}
+      </Suspense>
+    </Canvas>
+  );
+}
+
+export default SceneLogo2;
