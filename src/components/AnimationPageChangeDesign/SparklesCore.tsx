975e40c454b1155af108c4b87a16f24d350d7126
SerhiiOberemchuk <serhiioberemchuk@gmail.com>
2024-12-30 23:00:04 +0100

    Update SparklesCore.tsx

--- src/components/AnimationPageChangeDesign/SparklesCore.tsx
+++ src/components/AnimationPageChangeDesign/SparklesCore.tsx
@@ -80,7 +80,7 @@ export const SparklesCore = (props: ParticlesProps) => {
                   enable: false,
                   mode: 'repulse',
                 },
-                resize: true as any,
+                // resize: true as any,
               },
               modes: {
                 push: {
