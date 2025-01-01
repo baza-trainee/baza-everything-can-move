503e8b07a1bc21795db1762e4e7e91415fbbe42e
GannaKov <zlatta2000@gmail.com>
2025-01-01 18:00:02 +0100

    save

--- src/components/layout/Header/Shared/SceneLogo2.tsx
+++ src/components/layout/Header/Shared/SceneLogo2.tsx
@@ -6,7 +6,7 @@ import { Suspense } from 'react';
 
 function SceneLogo2() {
   return (
-    <Canvas gl={{ antialias: true }} dpr={[1, 1.5]}>
+    <Canvas gl={{ antialias: true }} dpr={[1, 1.5]} key="logo-header">
       <directionalLight position={[-5, -5, 5]} intensity={5} />
       <Suspense fallback={null}>
         {/* <Center position={[-0.5, -0.5, 0]}> */}
--- src/components/sections/Home/designeBySvitlana/Hero/SceneCopy.tsx
+++ src/components/sections/Home/designeBySvitlana/Hero/SceneCopy.tsx
@@ -6,7 +6,7 @@ import { Center } from '@react-three/drei';
 function SceneCopy() {
   return (
     <div className="absolute -top-44 left-[30%] z-10 h-[700px] w-[100%] p-0 lg:w-[80%] 2xl:w-[50%]">
-      <Canvas gl={{ antialias: true }} dpr={[1, 1.5]}>
+      <Canvas gl={{ antialias: true }} dpr={[1, 1.5]} key="robot-copy">
         <directionalLight position={[-5, -5, 5]} intensity={5} />
         <Suspense fallback={null}>
           <Center position={[-0.5, -0.5, 0]}>
