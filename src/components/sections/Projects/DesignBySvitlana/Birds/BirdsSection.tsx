489f8d15df7087c1cb96476108ba0417fa4d98ce
GannaKov <zlatta2000@gmail.com>
2025-01-03 19:23:44 +0100

    save

--- src/components/sections/Projects/DesignBySvitlana/Birds/BirdsSection.tsx
+++ src/components/sections/Projects/DesignBySvitlana/Birds/BirdsSection.tsx
@@ -5,9 +5,16 @@ const BirdsSection = () => {
   return (
     <section className="relative m-auto h-[803px] w-full overflow-hidden lg:h-[924px] 2xl:mt-0 2xl:h-[754px]">
       <div className="absolute left-[50%] top-[254px] w-[300px] translate-x-[-50%] text-center lg:top-[238px] lg:w-[409px] 2xl:left-80 2xl:top-[132px] 2xl:w-[450px] 2xl:text-left">
+        <div
+          className="absolute -bottom-[10px] -left-[25px] h-[154px] w-[349px] rounded-[70%] lg:-bottom-[30px] lg:-left-[37px] lg:h-[126px] lg:w-[484px] 2xl:-bottom-[40px] 2xl:-left-[80px] 2xl:h-[247px] 2xl:w-[543px] 2xl:rounded-[50%]"
+          style={{
+            background:
+              'radial-gradient(50% 50% at 50% 50%, #d91212 0%, rgba(217, 12, 12, 0.681) 71.6%, rgba(199, 17, 17, 0.4) 100%)',
+          }}
+        ></div>{' '}
         <AnimatedTitle
           title="Наші проєкти"
-          className="mb-6 lg:mb-[6px]"
+          className="pb-6 lg:pb-[6px]"
           wordClasses={['text-white', 'text-s-purple']}
         />
         <p className="font-third-family text-m font-regular leading-s-24 2xl:text-l 2xl:leading-s-36">
