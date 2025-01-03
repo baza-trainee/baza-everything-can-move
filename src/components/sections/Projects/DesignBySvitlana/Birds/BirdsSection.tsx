4488f9e464897062dc2aa00e0b7910211aa82fa0
GannaKov <zlatta2000@gmail.com>
2025-01-04 00:12:13 +0100

    save text size

--- src/components/sections/Projects/DesignBySvitlana/Birds/BirdsSection.tsx
+++ src/components/sections/Projects/DesignBySvitlana/Birds/BirdsSection.tsx
@@ -4,7 +4,7 @@ import AnimatedTitle from '@/components/ui/DesignBySvitlna/AnimatedTitle';
 const BirdsSection = () => {
   return (
     <section className="relative m-auto h-[803px] w-full overflow-hidden lg:h-[924px] 2xl:mt-0 2xl:h-[754px]">
-      <div className="absolute left-[50%] top-[254px] w-[300px] translate-x-[-50%] text-center lg:top-[238px] lg:w-[409px] 2xl:left-80 2xl:top-[132px] 2xl:w-[450px] 2xl:text-left">
+      <div className="absolute left-[50%] top-[254px] w-[340px] translate-x-[-50%] text-center lg:top-[238px] lg:w-[409px] 2xl:left-80 2xl:top-[132px] 2xl:w-[468px] 2xl:text-left">
         <div
           className="absolute -bottom-[10px] -left-[25px] -z-10 h-[154px] w-[349px] rounded-[70%] lg:-bottom-[30px] lg:-left-[37px] lg:h-[126px] lg:w-[484px] 2xl:-bottom-[40px] 2xl:-left-[80px] 2xl:h-[247px] 2xl:w-[543px] 2xl:rounded-[50%]"
           style={{
