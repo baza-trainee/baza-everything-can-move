6fa9f75480dda6c335ecf4a5d6dec71dde563c94
VitaliyPritula <pritula.vitaly94@gmail.com>
2025-01-08 15:21:16 +0200

    add

--- src/components/sections/Home/designeBySvitlana/Projects/Projects.tsx
+++ src/components/sections/Home/designeBySvitlana/Projects/Projects.tsx
@@ -1,4 +1,4 @@
-'use client'; // Додайте це, якщо використовуєте Next.js
+'use client'; 
 import Background from '@/components/ui/DesignBySvitlna/BackgroundComponent';
 import BtnAnimated from '@/components/ui/DesignBySvitlna/BtnAnimated';
 import Slider from './Slider';
@@ -6,14 +6,14 @@ import AnimatedTitle from '@/components/ui/DesignBySvitlna/AnimatedTitle';
 
 export default function Projects() {
   return (
-    <Background text="02 // Проєкти" bg="s-gray" xl={940} isNoContainer={true}>
+    <Background text="02 // Проєкти" bg="s-gray" xl={840} isNoContainer={true}>
       <section className="relative mb-32 overflow-visible px-0">
         <div className="container">
           <div className="mb-12 items-center justify-between gap-x-20 xl:flex flex-wrap">
             <div className="title">
               <AnimatedTitle title="Проєкти" className="lg-text-[76px]" />
               <p className="mb-4 text-md uppercase lg:text-lg 2xl:mb-0">
-                baza trainee ukraine
+                baza trainee ukraine цС ІЦ
               </p>
             </div>
             <div className="flex flex-col xl:-space-y-7 xl:space-x-[456px] max-w-[690px]">
