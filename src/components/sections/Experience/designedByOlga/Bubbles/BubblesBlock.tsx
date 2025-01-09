4ccb0e9ce8271074e578e5fa881bd62afece6038
Viktoria Vonsovych <vikvons@gmail.com>
2025-01-09 22:21:13 +0100

    experience/bubbles

--- src/components/sections/Experience/designedByOlga/Bubbles/BubblesBlock.tsx
+++ src/components/sections/Experience/designedByOlga/Bubbles/BubblesBlock.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 // import Bubbles3D from './Bubbles3D';
 import Bubbles2D from './Bubbles2D';
-// import Bubbles2Dgreen from './Bubbles2DGreen';
+import Bubbles2Dgreen from './Bubbles2Dgreen';
 import SubTitle from '@/components/ui/DesignByOlga/SubTitle';
 import SectionTitle from '@/components/ui/SharedDesigns/SectionTitle';
 
@@ -26,15 +26,15 @@ const BubblesBlock = ({ title, subtitle }: BubblesSectionProps) => {
   //  })
 
   return (
-    <>
-      <div className="px-4 lg:px-6 2xl:px-[120px] relative overflow-clip pb-[100px] lg:pb-20 2xl:pb-[64px]">
+    <div className='container relative px-4 lg:px-6 2xl:px-[120px] pb-[100px] lg:pb-20 2xl:pb-[64px]'>
+      {/* <div className="px-4 lg:px-6 2xl:px-[120px] pb-[100px] lg:pb-20 2xl:pb-[64px]"> */}
         <SectionTitle className='mt-[54px] lg:mt-20 2xl:mt-10'>{title}</SectionTitle>
         <SubTitle className='mt-1'>{subtitle}</SubTitle>
         <Bubbles2D />
         {/* <Bubbles2Dgreen /> */}
-      </div>
-      {/* <Bubbles3D /> */}
-    </>
+      {/* </div> */}
+      <Bubbles2Dgreen />
+    </div>
   );
 };
 
