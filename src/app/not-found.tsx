5d879a22d15c67fba818a0b625c2047608631ecc
GannaKov <zlatta2000@gmail.com>
2025-01-19 22:14:44 +0100

    save girl

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,39 +1,18 @@
 import React from 'react';
-// import { useDesignStore } from '@/useDesignStore';
-// import Container from '@/components/ui/DesignBySvitlna/Container';
+
 import WhiteGirl from '@/components/Girl/WhiteGirl';
-// import AnimatedTitle from '@/components/ui/DesignBySvitlna/AnimatedTitle';
-// import SectionTitle from '@/components/ui/SharedDesigns/SectionTitle';
 
 const NotFoundPage = () => {
   return (
-    <section className="py-20">
-      <div className="mx-auto flex items-center justify-center">
+    <section className="py-[200px]">
+      <div className="relative mx-auto mb-2 w-[350px]">
         <h1 className="text-center text-xl text-white">Ups ...</h1>
-        <div className="">
+        <div className="absolute -top-14 right-2 h-[200px] w-[200px]">
           <WhiteGirl />
         </div>
       </div>
 
       <p className="text-center text-lg">такої сторінки немає</p>
-      {/* <div className="h-[800px] w-full">
-        <WhiteGirl />
-      </div> */}
-
-      {/* {designType === 'designBySvitlana' && (
-        <Container>
-          <AnimatedTitle
-            title="not found"
-            //   className="max-w-[544px]"
-            wordClasses={['text-white', 'text-s-purple']}
-          />
-        </Container>
-      )}
-      {designType === 'designByOlga' && (
-        <div className="container">
-          <SectionTitle className="mb-8">not found</SectionTitle>
-        </div>
-      )} */}
     </section>
   );
 };
