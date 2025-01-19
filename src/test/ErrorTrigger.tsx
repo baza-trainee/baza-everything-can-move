4be3f04240399bd1b281cc523ef5bfccd19cc9bc
GannaKov <zlatta2000@gmail.com>
2025-01-19 23:50:14 +0100

    clean

--- src/test/ErrorTrigger.tsx
+++ src/test/ErrorTrigger.tsx
@@ -1,12 +1,10 @@
 'use client';
 
 export default function ErrorTrigger() {
-  // Бросаем ошибку
   if (true) {
     throw new Error('Це тестова помилка!');
   }
 
-  // Компонент должен вернуть JSX
   return (
     <div>
       Цей текст ніколи не буде показаний, оскільки вище відбувається помилка.
