48e4a408d55d38be2e4e7828e51d1b7dc1e60d76
VitaliyPritula <pritula.vitaly94@gmail.com>
2025-02-23 17:25:44 +0200

    add link to Baza Traine

--- src/components/ui/ContaktButton/ContaktButton.tsx
+++ src/components/ui/ContaktButton/ContaktButton.tsx
@@ -1,12 +1,14 @@
-"use client";
-import "./style.css";
+'use client';
+import './style.css';
 export default function ScrollButton() {
   return (
-    <button
-      // onClick={openModal}
-      type="button"
-      className="absolute lg:right-[20px] right-[-20px] btn-support mr-[24px] xl:h-[132px] xl:w-[132px] h-[88px] w-[88px] lg:bottom-[-50%] bottom-[-250%] animate-pulse rounded-full bg-accent text-center text-m font-medium uppercase text-white custom-shadow transition-all duration-300 hover:bg-hover flex items-center justify-center z-10">
-      contact
-    </button>
+    <div className="">
+      <a
+        href="http://baza-skill.com.ua/ua"
+        className="btn-support bg-accent custom-shadow hover:bg-hover absolute bottom-[-250%] right-[-20px] z-10 mr-[24px] flex h-[88px] w-[88px] animate-pulse items-center justify-center rounded-full text-center text-m font-medium uppercase text-white transition-all duration-300 lg:bottom-[-50%] lg:right-[20px] xl:h-[132px] xl:w-[132px]"
+      >
+        contact
+      </a>
+    </div>
   );
 }
